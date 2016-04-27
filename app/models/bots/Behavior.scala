dff31deadf585dfa678dad1b15a428f8eb70d22b
Andrew Catton <andrew@catton.ca>
2016-04-27 13:51:21 -0400

    playing with bot behavior in AWS lambda functions - just a canned lambda function for now

--- app/models/bots/Behavior.scala
+++ app/models/bots/Behavior.scala
@@ -1,13 +1,34 @@
 package models.bots
 
+import java.nio.charset.Charset
+
+import com.amazonaws.auth.{BasicAWSCredentials}
+import com.amazonaws.services.lambda.AWSLambdaClient
+import com.amazonaws.services.lambda.model.{InvocationType, InvokeRequest}
 import models.{IDs, Team}
+import play.api.Play
+import play.api.libs.json.Json
 import slick.driver.PostgresDriver.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 case class Behavior(id: String, team: Team, description: String) {
 
+  lazy val conf = Play.current.configuration
+
   def resultFor(params: Map[String, String]): String = {
-    params.getOrElse("0", "Hm?")
+    val content = params.getOrElse("0", "<nothing>")
+    val invokeRequest =
+      new InvokeRequest().
+        withFunctionName("echo").
+        withInvocationType(InvocationType.RequestResponse).
+        withPayload(s""" { "content": "$content" } """)
+    val credentials = new BasicAWSCredentials(conf.getString("aws.accessKey").get, conf.getString("aws.secretKey").get)
+    // blocking
+    val client = new AWSLambdaClient(credentials)
+    val result = client.invoke(invokeRequest)
+    val bytes = result.getPayload().array
+    val jsonString = new java.lang.String( bytes, Charset.forName("UTF-8") )
+    (Json.parse(jsonString) \ "result").get.as[String]
   }
 
 }
