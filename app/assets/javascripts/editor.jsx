8f9073cc2604b26875f831926b2a93e24f680866
Luke Andrews <luke@attaboy.ca>
2016-05-10 16:11:28 -0400

    Introduce React compiler to the server, and begin rendering a new editor component

---
+++ app/assets/javascripts/editor.jsx
@@ -0,0 +1,13 @@
+var Editor = React.createClass({
+  render: function() {
+    return (
+      <form>
+      </form>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Editor />,
+  document.getElementById('editorContainer')
+);
