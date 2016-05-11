1e67800e8e46091a3fc8324cb010b6c151fb7c18
Luke Andrews <luke@attaboy.ca>
2016-05-10 21:43:09 -0400

    For the function input, use a React-wrapped Codemirror and report changes to the names of the arguments

--- app/assets/javascripts/behavior_editor.jsx
+++ app/assets/javascripts/behavior_editor.jsx
@@ -7,6 +7,15 @@ var BehaviorEditor = React.createClass({
           <p>You may also choose a short code name for reference.</p>
           <BehaviorEditorDescription description={this.props.description} />
         </div>
+
+        <div className="form-field-group">
+          <p><strong>Specify what @ellipsis should do by writing a node.js function.</strong></p>
+          <p>If you want your behavior to collect input from the user before operation, specify
+          each required value as an argument to the function.</p>
+          <div className="form-field">
+            <BehaviorEditorCodeInput code={this.props.nodeFunction} lineNumbers />
+          </div>
+        </div>
       </form>
     );
   }
@@ -26,7 +35,7 @@ var BehaviorEditorDescription = React.createClass({
   },
 
   getCodeName: function() {
-    var stripped = this.state.value.toLowerCase().replace(/[^a-z\s]/g, '');
+    var stripped = this.state.value.toLowerCase().replace(/[^\w ]/g, '');
     return stripped.split(' ').slice(0,3).join('-');
   },
 
@@ -43,10 +52,54 @@ var BehaviorEditorDescription = React.createClass({
         <input type="text"
           className="form-input"
           placeholder="bang-two-coconuts"
-          readonly
+          readOnly
           value={this.getCodeName()}
         />
       </div>
+    );
+  }
+});
+
+var BehaviorEditorCodeInput = React.createClass({
+  getInitialState: function() {
+    return {
+      code: this.props.code
+    };
+  },
+
+  getArgumentNamesFromCode: function(code) {
+    var firstLine = code.split('\n')[0];
+    var matches = firstLine.match(/^function\((.+)\)/);
+    if (!matches || matches.length < 2) {
+      return [];
+    }
+    var args = matches[1].split(',').map(function(arg) {
+      return arg.replace(/(^\s*)|(\s*$)/g, ''); // trim spaces
+    });
+    return args.slice(0, args.length - 2); // last two arguments are reserved for onSuccess/onError
+  },
+
+  updateCode: function(newCode) {
+    var argumentNames = this.getArgumentNamesFromCode(newCode);
+    this.setState({
+      code: newCode,
+      argumentNames: argumentNames
+    });
+    console.log(argumentNames);
+  },
+
+  render: function() {
+    var options = {
+      lineNumbers: this.props.lineNumbers || false,
+      mode: "javascript",
+      viewportMargin: Infinity
+    };
+    return (
+      <Codemirror
+        value={this.state.code}
+        onChange={this.updateCode}
+        options={options}
+      />
     );
   }
 });
