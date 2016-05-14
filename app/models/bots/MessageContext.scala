0efa2b72ec2baeae929206bae3ca1705b81b841d
Andrew Catton <andrew@catton.ca>
2016-05-14 18:40:06 -0400

    Added behavior edit links in help

--- app/models/bots/MessageContext.scala
+++ app/models/bots/MessageContext.scala
@@ -1,7 +1,9 @@
 package models.bots
 
+import scala.concurrent.ExecutionContext
+
 trait MessageContext {
-  def sendMessage(text: String): Unit
+  def sendMessage(text: String)(implicit ec: ExecutionContext): Unit
   val name: String
   def userIdForContext: String
 }
--- app/models/bots/SlackContext.scala
+++ app/models/bots/SlackContext.scala
@@ -5,14 +5,18 @@ import models.bots.conversations.Conversation
 import slack.models.Message
 import slack.rtm.SlackRtmClient
 
+import scala.concurrent.ExecutionContext
+
 case class SlackContext(
                         client: SlackRtmClient,
                         profile: SlackBotProfile,
                         message: Message
                         ) extends MessageContext {
 
-  def sendMessage(text: String): Unit = {
-    client.sendMessage(message.channel, s"<@${message.user}>: $text")
+  def sendMessage(text: String)(implicit ec: ExecutionContext): Unit = {
+    client.apiClient.postChatMessage(message.channel, s"<@${message.user}>: $text")
+//    postChatMessage() uses formatting
+//    client.sendMessage(message.channel, s"<@${message.user}>: $text")
   }
 
   val name: String = Conversation.SLACK_CONTEXT
