74e588ce63607c11537bb835fec09e6153083b02
Luke Andrews <luke@attaboy.ca>
2016-05-17 12:35:56 -0400

    Stop parsing a JSON object and instead use require config to pass data to the behavior editor

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,17 @@
 requirejs.config({
-    paths:{
-        'react': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react',
-        'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom',
-    },
-    packages: [{
-      name: 'codemirror',
-      location: 'codemirror/',
-      main: 'lib/codemirror'
-    }]
+  paths:{
+    'react': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react',
+    'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom',
+  },
+  packages: [{
+    name: 'codemirror',
+    location: 'codemirror',
+    main: 'lib/codemirror'
+  }]
 });
 
-define(function(require) {
-  var myBehaviorEditor = require('./behavior_editor');
-  var data = JSON.parse(document.getElementById('editorData').text);
-  myBehaviorEditor.load(data);
+define(['module'], function(module) {
+  require(['behavior_editor'], function(behaviorEditor) {
+    behaviorEditor.load(module.config().data);
+  });
 });
