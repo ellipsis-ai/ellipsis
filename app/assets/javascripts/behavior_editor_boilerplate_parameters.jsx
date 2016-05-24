c67a4849f7c693d600a4af94447935851f1ee212
Luke Andrews <luke@attaboy.ca>
2016-05-24 11:17:15 -0400

    Normal indentation

--- app/assets/javascripts/behavior_editor_boilerplate_parameters.jsx
+++ app/assets/javascripts/behavior_editor_boilerplate_parameters.jsx
@@ -1,13 +1,13 @@
 define(function(require) {
-  var React = require('react');
+var React = require('react');
 
-  return React.createClass({
-    displayName: 'BehaviorEditorBoilerplateParameters',
-    render: function() {
-      return (
-        <code className="type-weak type-s">onSuccess,<br />onError,<br />ellipsis</code>
-      );
-    }
-  });
+return React.createClass({
+  displayName: 'BehaviorEditorBoilerplateParameters',
+  render: function() {
+    return (
+      <code className="type-weak type-s">onSuccess,<br />onError,<br />ellipsis</code>
+    );
+  }
+});
 
 });
