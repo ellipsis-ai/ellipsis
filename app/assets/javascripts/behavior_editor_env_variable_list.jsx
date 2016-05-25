63d5cc5a1c39bbe6c772eba1cf3461f09fc78637
Luke Andrews <luke@attaboy.ca>
2016-05-25 16:25:12 -0400

    Clean up the variable list expansion code a bit

--- app/assets/javascripts/behavior_editor_env_variable_list.jsx
+++ app/assets/javascripts/behavior_editor_env_variable_list.jsx
@@ -3,10 +3,24 @@ var React = require('react');
 
 return React.createClass({
   displayName: 'BehaviorEditorEnvVariableList',
+  lineHeight: 1.5,
+  isLastIndex: function(index) {
+    return index + 1 === this.props.envVariableNames.length;
+  },
+  maxHeightForVars: function() {
+    var height;
+    if (this.props.expandEnvVariables) {
+      // Add 2 extra lines for opening/closing braces, and 1 more for safety
+      height = this.lineHeight * (this.props.envVariableNames.length + 3);
+    } else {
+      height = this.lineHeight;
+    }
+    return height + 'em';
+  },
   render: function() {
     return (
       <div>
-        <h5 className="">
+        <h5>
           <span>{this.props.expandEnvVariables ? "▾" : "▸"}</span>
           <span> Current environment variables</span>
         </h5>
@@ -14,11 +28,7 @@ return React.createClass({
         <div className="phs pvxs bg-blue-lightest type-weak border border-blue">
           <div
             className="display display-ellipsis"
-            style={{
-              maxHeight: (this.props.expandEnvVariables ?
-                ((this.props.envVariableNames.length + 3) * 1.5) + 'em' :
-                '1.5em')
-            }}
+            style={{ maxHeight: this.maxHeightForVars() }}
           >
             <code className="prs">{'ellipsis.env: {'}</code>
             {this.props.envVariableNames.map(function(name, index) {
@@ -29,11 +39,13 @@ return React.createClass({
                 >
                   <code>{name + ': '}</code>
                   <span
-                    className={"bg-dark-translucent " + (this.props.expandEnvVariables ? "plxxxl" : "plm")}
-                    title="For security, environment variable values are not displayed."></span>
-                  <code>
-                    {index < this.props.envVariableNames.length - 1 ? ", " : " "}
-                  </code>
+                    className={
+                      "bg-dark-translucent " +
+                      (this.props.expandEnvVariables ? "plxxxl" : "plm")
+                    }
+                    title="For security, environment variable values are not displayed.">
+                  </span>
+                  <code>{this.isLastIndex(index) ? ", " : " "}</code>
                 </div>
               );
             }, this)}
