4aff397e67742282e5f0ec1ae56ae96565731417
Luke Andrews <luke@attaboy.ca>
2016-06-01 21:47:52 -0400

    Fix env var reveal animation by hand for now

--- app/assets/javascripts/behavior_editor_env_variable_list.jsx
+++ app/assets/javascripts/behavior_editor_env_variable_list.jsx
@@ -31,7 +31,7 @@ return React.createClass({
         <div className="phs pvxs bg-blue-lightest type-weak border border-blue">
           <div
             className="display-ellipsis"
-            style={{ maxHeight: this.maxHeightForVars() }}
+            style={{ maxHeight: this.maxHeightForVars(), transition: 'max-height 0.15s ease' }}
           >
             <code className="prs">{'ellipsis.env: {'}</code>
             {this.props.envVariableNames.map(function(name, index) {
