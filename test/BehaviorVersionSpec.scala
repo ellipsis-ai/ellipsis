803cd99b3bf57ee4af63a13cb4e52fe5a3e28941
Andrew Catton <andrew@catton.ca>
2016-06-16 13:42:04 -0400

    test triggers and params for restores too

--- test/BehaviorVersionSpec.scala
+++ test/BehaviorVersionSpec.scala
@@ -1,4 +1,5 @@
-import models.bots.{Behavior, BehaviorVersionQueries, BehaviorQueries}
+import models.bots.triggers.MessageTriggerQueries
+import models.bots.{BehaviorParameterQueries, Behavior, BehaviorVersionQueries, BehaviorQueries}
 import models.{Team, IDs}
 import org.scalatestplus.play.PlaySpec
 import slick.driver.PostgresDriver.api.{Database => PostgresDatabase}
@@ -33,16 +34,30 @@ class BehaviorVersionSpec extends PlaySpec with DBMixin {
         var firstVersion = runNow(db, BehaviorVersionQueries.createFor(behavior))
         firstVersion = runNow(db, firstVersion.copy(maybeFunctionBody = firstCode).save)
         runNow(db, reloadBehavior(db, behavior).maybeCurrentVersion).get.maybeFunctionBody mustBe firstCode
+        val firstTrigger = runNow(db, MessageTriggerQueries.createFor(firstVersion, "first", false, false, false))
+        runNow(db, MessageTriggerQueries.allFor(firstVersion)).map(_.pattern) mustBe Seq("first")
+        val firstParam = runNow(db, BehaviorParameterQueries.createFor("first", None, 1, firstVersion))
+        runNow(db, BehaviorParameterQueries.allFor(firstVersion)).map(_.name) mustBe Seq("first")
 
         var secondVersion = runNow(db, BehaviorVersionQueries.createFor(behavior))
         secondVersion = runNow(db, secondVersion.copy(maybeFunctionBody = secondCode).save)
         runNow(db, reloadBehavior(db, behavior).maybeCurrentVersion).get.maybeFunctionBody mustBe secondCode
+        val secondTrigger = runNow(db, MessageTriggerQueries.createFor(secondVersion, "second", false, false, false))
+        runNow(db, MessageTriggerQueries.allFor(secondVersion)).map(_.pattern) mustBe Seq("second")
+        val secondParam = runNow(db, BehaviorParameterQueries.createFor("second", None, 1, secondVersion))
+        runNow(db, BehaviorParameterQueries.allFor(secondVersion)).map(_.name) mustBe Seq("second")
 
         runNow(db, firstVersion.restore)
+        val restoredVersion = runNow(db, reloadBehavior(db, behavior).maybeCurrentVersion).get
+
         // code is restored
-        runNow(db, reloadBehavior(db, behavior).maybeCurrentVersion).get.maybeFunctionBody mustBe firstCode
+        restoredVersion.maybeFunctionBody mustBe firstCode
+        // triggers restored too
+        runNow(db, MessageTriggerQueries.allFor(restoredVersion)).map(_.pattern) mustBe Seq("first")
+        // and params
+        runNow(db, BehaviorParameterQueries.allFor(restoredVersion)).map(_.name) mustBe Seq("first")
         // new ID for new version instance
-        reloadBehavior(db, behavior).maybeCurrentVersionId mustNot be(Some(firstVersion.id))
+        restoredVersion.id mustNot be(Some(firstVersion.id))
       }
     }
 
