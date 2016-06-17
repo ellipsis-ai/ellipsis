c49131d1491c37990fb80158464d69855b956a0e
Andrew Catton <andrew@catton.ca>
2016-06-17 12:30:50 -0400

    Clean up log result code a bit

--- app/services/AWSLambdaLogResult.scala
+++ app/services/AWSLambdaLogResult.scala
@@ -4,25 +4,35 @@ case class AWSLambdaLogResult(source: String, userDefinedLogStatements: String, 
 
 object AWSLambdaLogResult {
 
-  def fromText(text: String, isInDevelopmentMode: Boolean): AWSLambdaLogResult = {
+  def extractErrorAndNonErrorContentFrom(text: String): (Option[String], String) = {
     var nonErrorContent = text
     var maybeErrorContent: Option[String] = None
     val extractErrorRegex = """(?s)(.*\n)\S+\t\S+\t(\S*Error:.*)\n[^\n]*\nEND.*""".r
     extractErrorRegex.findFirstMatchIn(text).foreach { m =>
       nonErrorContent = m.subgroups.head
-      maybeErrorContent = m.subgroups.tail.headOption.map( s => s"```$s```" )
+      maybeErrorContent = m.subgroups.tail.headOption.map(s => s"```$s```")
     }
-    val maybeUserDefinedLogStatementsContent = """(?s)START.*?\n(.*)""".r.findFirstMatchIn(nonErrorContent).flatMap(_.subgroups.headOption)
+    (maybeErrorContent, nonErrorContent)
+  }
+
+  def extractUserDefinedLogStatementsFrom(text: String): String = {
+    val maybeUserDefinedLogStatementsContent = """(?s)START.*?\n(.*)""".r.findFirstMatchIn(text).flatMap(_.subgroups.headOption)
+    maybeUserDefinedLogStatementsContent.map { content =>
+      content.split( """\S+\t\S+\t""")
+    }.map { strings =>
+      strings.
+        map(_.trim).
+        filter(_.nonEmpty)
+        .map(s => s"You logged: ```$s```\n").
+        mkString("")
+    }.getOrElse("")
+  }
+
+  def fromText(text: String, isInDevelopmentMode: Boolean): AWSLambdaLogResult = {
+    var (maybeErrorContent, nonErrorContent) = extractErrorAndNonErrorContentFrom(text)
+
     val userDefinedLogStatements = if (isInDevelopmentMode) {
-      maybeUserDefinedLogStatementsContent.map { content =>
-        content.split( """\S+\t\S+\t""")
-      }.map { strings =>
-        strings.
-          map(_.trim).
-          filter(_.nonEmpty)
-          .map(s => s"You logged: ```$s```\n").
-          mkString("")
-      }.getOrElse("")
+      extractUserDefinedLogStatementsFrom(nonErrorContent)
     } else {
       ""
     }
