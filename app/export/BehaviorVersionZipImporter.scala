3ef09dd3e496d037cbf3ae10791e55a59d119e14
Andrew Catton <andrew@catton.ca>
2016-06-26 21:50:08 -0400

    Proof of concept for importing behaviors from a github repo owned by us

---
+++ app/export/BehaviorVersionZipImporter.scala
@@ -0,0 +1,50 @@
+package export
+
+import java.io.{ByteArrayOutputStream, FileInputStream, File}
+import java.util.zip.{ZipEntry, ZipInputStream}
+
+import json.EditorFormat.BehaviorVersionData
+import models.Team
+import models.bots.BehaviorVersion
+import services.AWSLambdaService
+import slick.dbio.DBIO
+
+case class BehaviorVersionZipImporter(team: Team, lambdaService: AWSLambdaService, zipFile: File) {
+
+  private def readDataFrom(zipInputStream: ZipInputStream): String = {
+    val buffer = new Array[Byte](1024)
+    val out = new ByteArrayOutputStream()
+    var len: Int = zipInputStream.read(buffer)
+
+    while (len > 0) {
+      out.write(buffer, 0, len)
+      len = zipInputStream.read(buffer)
+    }
+
+    out.toString
+  }
+
+  def run: DBIO[BehaviorVersion] = {
+
+    val zipInputStream: ZipInputStream = new ZipInputStream(new FileInputStream(zipFile))
+    var entry: ZipEntry = zipInputStream.getNextEntry
+    val strings = scala.collection.mutable.Map[String, String]()
+
+    while (entry != null) {
+      strings.put(entry.getName, readDataFrom(zipInputStream))
+      entry = zipInputStream.getNextEntry
+    }
+
+    val data =
+      BehaviorVersionData.fromStrings(
+        team.id,
+        strings.getOrElse("function.js", ""),
+        strings.getOrElse("response.md", ""),
+        strings.getOrElse("params.json", ""),
+        strings.getOrElse("triggers.json", "")
+      )
+
+    BehaviorVersionImporter(team, lambdaService, data).run
+  }
+
+}
