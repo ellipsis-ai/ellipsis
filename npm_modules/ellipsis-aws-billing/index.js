2271f5ba44e767dc5c81a538b9c406aeefe42c54
Andrew Catton <andrew@catton.ca>
2016-07-01 22:28:35 -0400

    Support promise-based interface in ellipsis-aws-billing

--- npm_modules/ellipsis-aws-billing/index.js
+++ npm_modules/ellipsis-aws-billing/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const Q = require('q');
+
 const cleanUpValue = (v) => {
   return v.replace(/^\"|\"$/g, "");
 };
@@ -8,47 +10,56 @@ module.exports = function(s3, bucketName) {
 
   return {
 
-    withCsvReports: function(cb) {
+    csvReports: function(onSuccess, onError) {
       const params = {
         "Bucket": bucketName
       };
       s3.listObjectsV2(params, (err, data) => {
-        if (data) {
+        if (err) {
+          onError(err);
+        } else {
           const csvRegex = /\d+-aws-billing-csv-\d\d\d\d-\d\d/;
           const csvKeys =
             data.Contents.
               map((ea) => ea.Key).
               filter((ea) => csvRegex.test(ea));
           const reverseChrono = csvKeys.sort().reverse();
-          cb(reverseChrono);
-        } else {
-          cb([]);
+          onSuccess(reverseChrono);
         }
       });
     },
 
-    withTotalFor: function(csvReportKey, cb) {
+    csvReportsPromise: function() {
+      const deferred = Q.defer();
+      this.csvReports(deferred.resolve, deferred.reject);
+      return deferred.promise;
+    },
+
+    totalFor: function(csvReportKey, onSuccess, onError) {
       const getObjectParams = {
         "Bucket": bucketName,
         "Key": csvReportKey
       };
       s3.getObject(getObjectParams, (err, data) => {
         if (err) {
-          cb({
-            currency: "",
-            amount: "N/A"
-          });
+          onError(err);
         } else {
           const lines = data.Body.toString("utf8").split("\n");
           const totalValues = lines[lines.length-3].split(",");
           const currency = cleanUpValue(totalValues[totalValues.length-6]);
           const amount = cleanUpValue(totalValues[totalValues.length-1]);
-          cb({
+          onSuccess({
             currency: currency,
             amount: amount
           });
         }
       });
+    },
+
+    totalForPromise: function(csvReportKey) {
+      const deferred = Q.defer();
+      this.totalFor(csvReportKey, deferred.resolve, deferred.reject);
+      return deferred.promise;
     }
 
   };
