8fbee39140b64f9961dd6fb2d8d426c6a7a32089
Luke Andrews <luke@attaboy.ca>
2016-07-03 17:48:02 -0400

    Rename isInstalling to isImporting

--- app/assets/javascripts/behavior_importer_behavior.jsx
+++ app/assets/javascripts/behavior_importer_behavior.jsx
@@ -22,7 +22,7 @@ define(function(require) {
     },
 
     getLocalBehaviorEditLink: function() {
-      if (this.isInstalling()) {
+      if (this.isImporting()) {
         return (
           <span className="mhm fade-in type-weak type-bold">Installing…</span>
         );
@@ -46,7 +46,7 @@ define(function(require) {
     },
 
     getInstallButton: function() {
-      if (this.isInstalling()) {
+      if (this.isImporting()) {
         return (
           <button type="button" className="button-raw button-s">
             <SVGInstalling />
@@ -71,7 +71,7 @@ define(function(require) {
       return !!this.props.isImported;
     },
 
-    isInstalling: function() {
+    isImporting: function() {
       return this.state.importing;
     },
 
@@ -110,7 +110,7 @@ define(function(require) {
           <input type="hidden" name="csrfToken" value={this.props.csrfToken} />
           <input type="hidden" name="teamId" value={this.props.behaviorData.teamId} />
           <input type="hidden" name="dataJson" value={JSON.stringify(this.props.behaviorData)} />
-          <div className={"columns columns-elastic mbm " + (this.isInstalling() ? "pulse" : "")}>
+          <div className={"columns columns-elastic mbm " + (this.isImporting() ? "pulse" : "")}>
             <div className="column column-shrink">
               {this.getInstallButton()}
             </div>
