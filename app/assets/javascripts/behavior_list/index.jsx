f8d65221a4d7e5c79037fba050eed99310888540
Luke Andrews <luke@attaboy.ca>
2016-07-19 16:24:15 -0400

    Group behaviors in the list by task vs knowledge depending on whether they include code

--- app/assets/javascripts/behavior_list/index.jsx
+++ app/assets/javascripts/behavior_list/index.jsx
@@ -7,10 +7,15 @@ define(function(require) {
       behaviorVersions: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
     },
 
-    getFirstTriggerFromVersion: function(version) {
+    getFirstTriggerTextFromVersion: function(version) {
       var firstTrigger = version.triggers[0];
-      var text = firstTrigger && firstTrigger.text ?
-        (<span className="type-monospace">{firstTrigger.text}</span>) :
+      return firstTrigger && firstTrigger.text ? firstTrigger.text : "";
+    },
+
+    getFirstTriggerFromVersion: function(version) {
+      var firstTriggerText = this.getFirstTriggerTextFromVersion(version);
+      var text = firstTriggerText ?
+        (<span className="type-monospace">{firstTriggerText}</span>) :
         (<span className="type-italic">(New behavior)</span>);
       return (
         <a href={jsRoutes.controllers.ApplicationController.editBehavior(version.behaviorId).url}>{text}</a>
@@ -41,29 +46,95 @@ define(function(require) {
       );
     },
 
+    getGroupedVersions: function() {
+      var tasks = [];
+      var knowledge = [];
+      this.props.behaviorVersions.forEach(function(version) {
+        if (version.functionBody) {
+          tasks.push(version);
+        } else {
+          knowledge.push(version);
+        }
+      }, this);
+      return {
+        tasks: this.sortVersionsByFirstTrigger(tasks),
+        knowledge: this.sortVersionsByFirstTrigger(knowledge)
+      };
+    },
+
+    sortVersionsByFirstTrigger: function(versions) {
+      return versions.sort(function(version1, version2) {
+        var t1 = this.getFirstTriggerTextFromVersion(version1);
+        var t2 = this.getFirstTriggerTextFromVersion(version2);
+        if (t1 < t2) {
+          return -1;
+        } else if (t1 > t2) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }.bind(this));
+    },
+
+    getKnowledge: function() {
+      return this.state.groupedVersions.knowledge;
+    },
+
+    getTasks: function() {
+      return this.state.groupedVersions.tasks;
+    },
+
+    getInitialState: function() {
+      return {
+        groupedVersions: this.getGroupedVersions()
+      }
+    },
+
+    getTableRowClasses: function(index) {
+      if (index % 3 === 0) {
+        return " pts border-top ";
+      } else if (index % 3 === 2) {
+        return " pbs ";
+      } else {
+        return "";
+      }
+    },
+
+    getVersionRow: function(version, index) {
+      return (
+        <tr key={"version" + index}>
+          <td className={this.getTableRowClasses(index)}>
+          </td>
+          <td className={"type-s" + this.getTableRowClasses(index)}>
+            {this.getTriggersFromVersion(version)}
+          </td>
+          <td className={"type-s align-r" + this.getTableRowClasses(index)}>
+            {Formatter.formatTimestampShort(version.createdAt)}
+          </td>
+        </tr>
+      );
+    },
+
     render: function() {
       return (
         <table>
           <thead>
             <tr>
-              <th className="type-label pbs"></th>
-              <th className="type-label pbs">When someone says</th>
-              <th className="type-label pbs align-r">Last modified</th>
+              <th className="ptl type-l pbs" colSpan="2">What Ellipsis can do</th>
+              <th className="type-label align-r pbs">Last modified</th>
             </tr>
           </thead>
           <tbody>
-            {this.props.behaviorVersions.map(function(version, index) {
-              return (
-                <tr key={"version" + index}>
-                  <td className="pvs border-top">
-                  </td>
-                  <td className="pvs border-top">{this.getTriggersFromVersion(version)}</td>
-                  <td className="pvs border-top type-s align-r">
-                    {Formatter.formatTimestampShort(version.createdAt)}
-                  </td>
-                </tr>
-              );
-            }, this)}
+            {this.getTasks().map(this.getVersionRow, this)}
+          </tbody>
+          <thead>
+            <tr>
+              <th className="ptxxl type-l pbs" colSpan="2">What Ellipsis knows</th>
+              <th className="type-label align-r pbs">Last modified</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.getKnowledge().map(this.getVersionRow, this)}
           </tbody>
         </table>
       );
