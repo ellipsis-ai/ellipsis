499a5ae867d40788822b8f327da71bfb063349fd
Andrew Catton <andrew@catton.ca>
2016-08-04 17:46:21 -0400

    request json response

--- app/models/accounts/CustomProvider.scala
+++ app/models/accounts/CustomProvider.scala
@@ -23,14 +23,19 @@ class CustomProvider(
 
   override val profileParser = new CustomProfileParser(configuration)
 
+  override protected val headers: Seq[(String, String)] = Seq(("Accept", "application/json"))
+
   override def withSettings(f: (Settings) => Settings) = new CustomProvider(configuration, httpLayer)
 
   protected def urls: Map[String, String] = Map("getProfile" -> configuration.getProfileUrl)
 
   protected def buildProfile(authInfo: A): Future[CustomProfile] = {
-    httpLayer.url(urls("getProfile").format(authInfo.accessToken)).get().flatMap { response =>
-      profileParser.parse(response.json)
-    }
+    httpLayer.url(urls("getProfile").format(authInfo.accessToken)).
+      withHeaders(headers: _*).
+      get().
+      flatMap { response =>
+        profileParser.parse(response.json)
+      }
   }
 
 }
