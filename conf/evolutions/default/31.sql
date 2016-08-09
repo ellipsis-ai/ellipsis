f9e79ddb4ea0bebb35cc339eb2e37389507c96a4
Andrew Catton <andrew@catton.ca>
2016-08-09 12:41:52 -0400

    Safer down migration

--- conf/evolutions/default/31.sql
+++ conf/evolutions/default/31.sql
@@ -5,5 +5,5 @@ ALTER TABLE custom_oauth2_configuration_templates DROP COLUMN get_profile_json_p
 
 # --- !Downs
 
-ALTER TABLE custom_oauth2_configuration_templates ADD COLUMN get_profile_json_path TEXT NOT NULL;
-ALTER TABLE custom_oauth2_configuration_templates ADD COLUMN get_profile_url TEXT NOT NULL;
+ALTER TABLE custom_oauth2_configuration_templates ADD COLUMN get_profile_json_path TEXT NOT NULL DEFAULT "";
+ALTER TABLE custom_oauth2_configuration_templates ADD COLUMN get_profile_url TEXT NOT NULL DEFAULT "";
