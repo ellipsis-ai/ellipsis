6728402dc23ee768b1649c1de0b2a15e9cbb09f1
Andrew Catton <andrew@catton.ca>
2016-09-02 14:47:25 -0400

    Put non-auth-related error handling back in ErrorHandler

--- app/utils/CustomSecuredErrorHandler.scala
+++ app/utils/CustomSecuredErrorHandler.scala
@@ -4,7 +4,6 @@ import javax.inject.Inject
 
 import com.google.inject.Singleton
 import com.mohiva.play.silhouette.api.actions.SecuredErrorHandler
-import play.api.UsefulException
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{RequestHeader, Result, Results}
 
@@ -29,43 +28,4 @@ class CustomSecuredErrorHandler @Inject() (
   def onNotAuthorized(implicit request: RequestHeader): Future[Result] = {
     Future.successful(Results.Ok("not authorized"))
   }
-
-  def onNotFound(request: RequestHeader, message: String): Future[Result] = {
-    implicit val r = request
-    val maybeNonEmptyMessage = Option(message).filter(_.trim.nonEmpty)
-    Future.successful(
-      Results.NotFound(
-        views.html.notFound(
-          None,
-          None,
-          maybeNonEmptyMessage
-        )
-      )
-    )
-  }
-
-  def onProdServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
-    implicit val r = request
-    Future.successful(
-      Results.InternalServerError(
-        views.html.serverError(
-          Some(exception.id)
-        )
-      )
-    )
-  }
-
-  /* Uncomment to test the custom error handler locally
-  def onDevServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
-    implicit val r = request
-    implicit val messages = messagesApi.preferred(request)
-    Future.successful(
-      InternalServerError(
-        views.html.serverError(
-          Some(exception.id)
-        )
-      )
-    )
-  }
-  */
 }
---
+++ app/utils/ErrorHandler.scala
@@ -0,0 +1,63 @@
+package utils
+
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpErrorHandler
+import play.api.i18n.MessagesApi
+import play.api.mvc.Results._
+import play.api.mvc.{RequestHeader, Result}
+import play.api.routing.Router
+import play.api.{Configuration, OptionalSourceMapper, UsefulException}
+
+import scala.concurrent.Future
+
+class ErrorHandler @Inject() (
+                               env: play.api.Environment,
+                               config: Configuration,
+                               sourceMapper: OptionalSourceMapper,
+                               router: javax.inject.Provider[Router],
+                               messagesApi: MessagesApi
+                             )
+  extends DefaultHttpErrorHandler(env, config, sourceMapper, router) {
+
+  override def onNotFound(request: RequestHeader, message: String): Future[Result] = {
+    implicit val r = request
+    implicit val m = messagesApi.preferred(request)
+    val maybeNonEmptyMessage = Option(message).filter(_.trim.nonEmpty)
+    Future.successful(
+      NotFound(
+        views.html.notFound(
+          None,
+          None,
+          maybeNonEmptyMessage
+        )
+      )
+    )
+  }
+
+  override def onProdServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
+    implicit val r = request
+    implicit val messages = messagesApi.preferred(request)
+    Future.successful(
+      InternalServerError(
+        views.html.serverError(
+          Some(exception.id)
+        )
+      )
+    )
+  }
+
+  /* Uncomment to test the custom error handler locally
+  override def onDevServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
+    implicit val r = request
+    implicit val messages = messagesApi.preferred(request)
+    Future.successful(
+      InternalServerError(
+        views.html.serverError(
+          Some(exception.id)
+        )
+      )
+    )
+  }
+  */
+}
