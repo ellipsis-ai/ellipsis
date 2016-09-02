58af06561bf6aad982a0a41d7017bbfbe223bade
Andrew Catton <andrew@catton.ca>
2016-09-01 23:55:38 -0400

    Fix a broken test by injecting the DataService

--- test/BehaviorVersionSpec.scala
+++ test/BehaviorVersionSpec.scala
@@ -1,11 +1,15 @@
+import javax.inject.Inject
+
 import models.accounts.user.User
 import models.bots.{Behavior, BehaviorQueries, BehaviorVersionQueries}
 import models.{IDs, Team}
 import org.scalatestplus.play.PlaySpec
+import services.DataService
+import slick.dbio.DBIO
 import slick.driver.PostgresDriver.api.{Database => PostgresDatabase}
 
 
-class BehaviorVersionSpec extends PlaySpec with DBMixin {
+class BehaviorVersionSpec @Inject() (dataService: DataService) extends PlaySpec with DBMixin {
 
   def reloadBehavior(db: PostgresDatabase, behavior: Behavior): Behavior = {
     runNow(db, BehaviorQueries.findWithoutAccessCheck(behavior.id)).get
@@ -16,7 +20,7 @@ class BehaviorVersionSpec extends PlaySpec with DBMixin {
     "should load the current version" in {
       withDatabase { db =>
         val team = runNow(db, Team(IDs.next, "").save)
-        val user = runNow(db, User(IDs.next, team.id, None).save)
+        val user = runNow(db, DBIO.from(dataService.users.save(User(IDs.next, team.id, None))))
         val behavior = runNow(db, BehaviorQueries.createFor(team, None))
         val firstVersion = runNow(db, BehaviorVersionQueries.createFor(behavior, Some(user)))
         reloadBehavior(db, behavior).maybeCurrentVersionId mustBe Some(firstVersion.id)
