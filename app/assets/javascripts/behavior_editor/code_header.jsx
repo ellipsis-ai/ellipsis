54f4b7145f1ec9487fe395789ab6166fd6c0c92b
Luke Andrews <luke@attaboy.ca>
2016-09-19 21:25:38 -0400

    Remove some unused code on codeheader Nicer spacing/formatting

--- app/assets/javascripts/behavior_editor/code_header.jsx
+++ app/assets/javascripts/behavior_editor/code_header.jsx
@@ -1,25 +1,20 @@
 define(function(require) {
 var React = require('react'),
-  BehaviorEditorMixin = require('./behavior_editor_mixin'),
   HelpButton = require('../help/help_button');
 
 return React.createClass({
-  mixins: [BehaviorEditorMixin],
   propTypes: {
     helpVisible: React.PropTypes.bool,
     onToggleHelp: React.PropTypes.func.isRequired,
     userParams: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
     systemParams: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
   },
-  boilerplateLineNumber: function() {
-    return 1;
-  },
   boilerplateLine: function() {
     return this.props.systemParams.join(", ");
   },
   render: function() {
     return (
-      <div className="mbxs">
+      <div className="pbxs">
         <div className="columns columns-elastic">
           <div className="column column-shrink plxxxl prn align-r position-relative">
             <code className="type-disabled type-s position-absolute position-top-right prxs">1</code>
@@ -28,10 +23,11 @@ return React.createClass({
             <code className="type-s">
               <span className="type-s type-weak">{"function ("}</span>
               {this.props.userParams.map((param, paramIndex) => (
-                <span key={`param${paramIndex}`}>{param.name}, </span>
+                <span key={`param${paramIndex}`}>{param.name}<span className="type-weak">, </span></span>
               ))}
-              <span className="type-weak">{this.boilerplateLine()}</span> <HelpButton onClick={this.props.onToggleHelp} toggled={this.props.helpVisible} />
-              <span className="type-weak">{") {"}</span>
+              <span className="type-weak mrxs">{this.boilerplateLine()}</span>
+              <HelpButton onClick={this.props.onToggleHelp} toggled={this.props.helpVisible} />
+              <span className="type-weak mlxs">{") {"}</span>
             </code>
           </div>
         </div>
