541531e56d899f0d34a744989509eed165e7c8de
Andrew Catton <andrew@catton.ca>
2016-09-29 09:32:29 -0400

    Add the ability to import a behavior-backed data type from a zip file - to test export/import generally and for manually moving around data types

--- app/controllers/BehaviorBackedDataTypeController.scala
+++ app/controllers/BehaviorBackedDataTypeController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.Silhouette
-import export.BehaviorBackedDataTypeExporter
+import export.{BehaviorBackedDataTypeExporter, BehaviorBackedDataTypeZipImporter}
 import models.silhouette.EllipsisEnv
 import play.api.data.Form
 import play.api.data.Forms._
@@ -74,6 +74,56 @@ class BehaviorBackedDataTypeController @Inject() (
         NotFound(s"Behavior not found: $id")
       }
     }
+  }
+
+  def importZip(maybeTeamId: Option[String]) = silhouette.SecuredAction.async { implicit request =>
+    val user = request.identity
+    dataService.users.teamAccessFor(user, maybeTeamId).map { teamAccess =>
+      teamAccess.maybeTargetTeam.map { team =>
+        Ok(views.html.importBehaviorBackedDataTypeZip(teamAccess))
+      }.getOrElse {
+        NotFound(s"No accessible team")
+      }}
+
+  }
+
+  case class ImportBehaviorZipInfo(teamId: String)
+
+  private val importBehaviorBackedDataTypeZipForm = Form(
+    mapping(
+      "teamId" -> nonEmptyText
+    )(ImportBehaviorZipInfo.apply)(ImportBehaviorZipInfo.unapply)
+  )
+
+  def doImportZip() = silhouette.SecuredAction.async { implicit request =>
+    (for {
+      formData <- request.body.asMultipartFormData
+      zipFile <- formData.file("zipFile")
+    } yield {
+      importBehaviorBackedDataTypeZipForm.bindFromRequest.fold(
+        formWithErrors => {
+          Future.successful(BadRequest(formWithErrors.errorsAsJson))
+        },
+        info => {
+          for {
+            maybeTeam <- dataService.teams.find(info.teamId, request.identity)
+            maybeImporter <- Future.successful(maybeTeam.map { team =>
+              BehaviorBackedDataTypeZipImporter(team, request.identity, zipFile.ref.file, dataService)
+            })
+            maybeDataType <- maybeImporter.map { importer =>
+              importer.run.map(Some(_))
+            }.getOrElse(Future.successful(None))
+          } yield {
+            maybeDataType.map { dataType =>
+              Redirect(routes.BehaviorBackedDataTypeController.list(Some(info.teamId)))
+            }.getOrElse {
+              NotFound(s"Team not found: ${info.teamId}")
+            }
+          }
+        }
+      )
+    }).getOrElse(Future.successful(BadRequest("")))
+
   }
 
 }
