cde73d7dd26c037df3945eb2656444846264b31f
Luke Andrews <luke@attaboy.ca>
2016-10-03 13:48:40 -0400

    Fewer lines for reserved JS words

--- app/assets/javascripts/models/param.jsx
+++ app/assets/javascripts/models/param.jsx
@@ -1,39 +1,10 @@
 define(function() {
 
   var reservedWords = Object.freeze([
-    'break',
-    'case',
-    'catch',
-    'class',
-    'const',
-    'continue',
-    'debugger',
-    'default',
-    'delete',
-    'do',
-    'else',
-    'export',
-    'extends',
-    'finally',
-    'for',
-    'function',
-    'if',
-    'import',
-    'in',
-    'instanceof',
-    'new',
-    'return',
-    'super',
-    'switch',
-    'this',
-    'throw',
-    'try',
-    'typeof',
-    'var',
-    'void',
-    'while',
-    'with',
-    'yield'
+    'break', 'case', 'catch', 'class', 'const', 'continue', 'debugger', 'default',
+    'delete', 'do', 'else', 'export', 'extends', 'finally', 'for', 'function',
+    'if', 'import', 'in', 'instanceof', 'new', 'return', 'super', 'switch',
+    'this', 'throw', 'try', 'typeof', 'var', 'void', 'while', 'with', 'yield'
   ]);
 
   class Param {
