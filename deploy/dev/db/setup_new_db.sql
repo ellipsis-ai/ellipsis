5ecb2a4bfd40800d5b9d1044203b0f1093f0c035
Matteo <m@ellipsis.ai>
2016-10-17 15:36:47 -0700

    checkin the sql script to setup the dev db

---
+++ deploy/dev/db/setup_new_db.sql
@@ -0,0 +1,5 @@
+CREATE USER ellipsis WITH PASSWORD 'ellipsis';
+CREATE DATABASE ellipsis;
+GRANT ALL PRIVILEGES ON DATABASE ellipsis TO ellipsis;
+CREATE DATABASE "ellipsis-test";
+GRANT ALL PRIVILEGES ON DATABASE "ellipsis-test" TO ellipsis;
