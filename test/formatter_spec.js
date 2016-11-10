6b31bc058b49b9fccf00890000c87b0b9c973da8
Luke Andrews <luke@attaboy.ca>
2016-11-10 12:22:29 -0500

    Display the code-compatible name for oauth2 applications

---
+++ test/formatter_spec.js
@@ -0,0 +1,15 @@
+jest.unmock('../app/assets/javascripts/formatter');
+
+const Formatter = require('../app/assets/javascripts/formatter');
+
+describe("Formatter", () => {
+  describe("formatCamelCaseIdentifier", () => {
+    it("converts a phrase of words into a camel-case word", () => {
+      const human = "The quick brown FOX — it jumped over the lazy dog!";
+      expect(Formatter.formatCamelCaseIdentifier(human)).toEqual("theQuickBrownFOXItJumpedOverTheLazyDog");
+    });
+    it('ensures the first character is valid', () => {
+      expect(Formatter.formatCamelCaseIdentifier('1 for the money')).toEqual("_1ForTheMoney");
+    })
+  });
+});
