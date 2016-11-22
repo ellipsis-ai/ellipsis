3c5716b4e8f39555b45f029e22ec4ca49a41e080
Luke Andrews <luke@attaboy.ca>
2016-11-22 15:52:46 -0500

    Change page heading padding to be on the title only, so children can set their own padding

--- app/assets/javascripts/behavior_editor/page_heading.jsx
+++ app/assets/javascripts/behavior_editor/page_heading.jsx
@@ -11,14 +11,14 @@ define(function(require) {
     render: function() {
       return (
         <div className="bg-light">
-          <div className="container pbs">
+          <div className="container">
             <div className="columns">
-              <div className="column column-one-quarter ptxl">
+              <div className="column column-one-quarter ptxl pbs">
                 <h3 className="mvn type-weak">
                   {this.props.heading}
                 </h3>
               </div>
-              <div className="column column-three-quarters pll ptl pbs">
+              <div className="column column-three-quarters pll">
                 {this.props.children}
               </div>
             </div>
