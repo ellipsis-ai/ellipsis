9c4a1c01dbecf0532f2211a76d21c657d8d10faa
Luke Andrews <luke@attaboy.ca>
2016-11-23 01:49:02 -0500

    Refactor the code that displays a behavior name in a list, so it can be re-used. This introduces a proper BehaviorVersion model class (the previous version was just a plain object), and augments the Trigger model.

---
+++ app/assets/javascripts/behavior_list/behavior_name.jsx
@@ -0,0 +1,89 @@
+define(function(require) {
+  var React = require('react'),
+    BehaviorVersion = require('../models/behavior_version'),
+    ImmutableObjectUtils = require('../immutable_object_utils');
+
+  return React.createClass({
+    displayName: 'Behavior',
+    propTypes: {
+      version: React.PropTypes.instanceOf(BehaviorVersion).isRequired
+    },
+
+    getLabelFromTrigger: function(trigger) {
+      return trigger && trigger.text ?
+        (<span className="link type-monospace">{trigger.displayText}</span>) :
+        (<span className="link type-italic">(New skill)</span>);
+    },
+
+    getNonRegexTriggerLabelsFromTriggers: function(triggers) {
+      return triggers.filter(function (trigger) {
+        return !trigger.isRegex;
+      }).map(function (trigger, index) {
+        if (trigger.text) {
+          return (
+            <span className="type-monospace" key={"regularTrigger" + index}>
+              <span className="type-disabled"> · </span>
+              <span>{trigger.displayText}</span>
+            </span>
+          );
+        } else {
+          return null;
+        }
+      }, this);
+    },
+
+    getRegexTriggerLabelFromTriggers: function(triggers) {
+      var regexTriggerCount = triggers.filter(function(trigger) {
+        return !!trigger.isRegex;
+      }).length;
+
+      var text = regexTriggerCount === 1 ?
+        "also matches another pattern" :
+      "also matches " + regexTriggerCount + " other patterns";
+
+      if (regexTriggerCount > 0) {
+        return (
+          <span>
+            <span className="type-monospace type-disabled"> · </span>
+            <span className="type-italic">{text}</span>
+          </span>
+        );
+      } else {
+        return null;
+      }
+    },
+
+    getTriggersFromVersion: function(version) {
+      var firstTriggerIndex = version.findFirstTriggerIndexForDisplay();
+      var firstTrigger = version.triggers[firstTriggerIndex];
+      var otherTriggers = ImmutableObjectUtils.arrayRemoveElementAtIndex(version.triggers, firstTriggerIndex);
+      return (
+        <div>
+          <a href={jsRoutes.controllers.BehaviorEditorController.edit(version.behaviorId).url}
+            className="link-block">
+            {this.getLabelFromTrigger(firstTrigger)}
+            {this.getNonRegexTriggerLabelsFromTriggers(otherTriggers)}
+            {this.getRegexTriggerLabelFromTriggers(otherTriggers)}
+          </a>
+        </div>
+      );
+    },
+
+    getDescriptionFromVersion: function(version) {
+      if (version.description) {
+        return (
+          <div className="type-italic type-weak pbxs ">{version.description}</div>
+        );
+      }
+    },
+
+    render: function() {
+      return (
+        <div>
+          {this.getTriggersFromVersion(this.props.version)}
+          {this.getDescriptionFromVersion(this.props.version)}
+        </div>
+      );
+    }
+  });
+});
--- app/assets/javascripts/behavior_list/loader.jsx
+++ app/assets/javascripts/behavior_list/loader.jsx
@@ -1,9 +1,15 @@
 /* global BehaviorListConfig:false */
 
 requirejs(['../common'], function() {
-  requirejs(['react', 'react-dom', './behavior_list/index'], function(React, ReactDOM, BehaviorList) {
-    var config = BehaviorListConfig;
-    var myBehaviorList = React.createElement(BehaviorList, config);
-    ReactDOM.render(myBehaviorList, document.getElementById(config.containerId));
-  });
+  requirejs(
+    ['react', 'react-dom', './behavior_list/index', './models/behavior_version'],
+    function(React, ReactDOM, BehaviorList, BehaviorVersion) {
+      var config = BehaviorListConfig;
+      var behaviorListProps = Object.assign({}, config, {
+        behaviorVersions: config.behaviorVersions.map((ea) => BehaviorVersion.fromJson(ea))
+      });
+      var myBehaviorList = React.createElement(BehaviorList, behaviorListProps);
+      ReactDOM.render(myBehaviorList, document.getElementById(config.containerId));
+    }
+  );
 });
