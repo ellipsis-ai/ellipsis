c56d4089e28857361a920ca005b679dfeb6bb853
Luke Andrews <luke@attaboy.ca>
2016-12-01 15:36:57 -0500

    Always require whatwg-fetch in loader.js files, and include it in common JS loaded into every build.

--- app/assets/javascripts/behavior_list/loader.jsx
+++ app/assets/javascripts/behavior_list/loader.jsx
@@ -2,8 +2,8 @@
 
 requirejs(['../common'], function() {
   requirejs(
-    ['react', 'react-dom', './behavior_list/index', './models/behavior_group'],
-    function(React, ReactDOM, BehaviorList, BehaviorGroup) {
+    ['core-js', 'whatwg-fetch', 'react', 'react-dom', './behavior_list/index', './models/behavior_group'],
+    function(Core, Fetch, React, ReactDOM, BehaviorList, BehaviorGroup) {
       var config = BehaviorListConfig;
       var behaviorListProps = Object.assign({}, config, {
         behaviorGroups: config.behaviorGroups.map((ea) => BehaviorGroup.fromJson(ea))
--- app/assets/javascripts/environment_variables/loader.jsx
+++ app/assets/javascripts/environment_variables/loader.jsx
@@ -1,7 +1,8 @@
 /* global EnvironmentVariableListConfig:false */
 
 requirejs(['../common'], function() {
-  requirejs(['core-js', 'react', 'react-dom', './environment_variables/index'], function(Core, React, ReactDOM, EnvironmentVariableList) {
+  requirejs(['core-js', 'whatwg-fetch', 'react', 'react-dom', './environment_variables/index'],
+    function(Core, Fetch, React, ReactDOM, EnvironmentVariableList) {
     var config = EnvironmentVariableListConfig;
     var myEnvironmentVariableList = React.createElement(EnvironmentVariableList, config);
     ReactDOM.render(myEnvironmentVariableList, document.getElementById(config.containerId));
