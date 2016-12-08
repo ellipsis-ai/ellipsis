f898cd54230c033342e47da4894ec2f473a5e7ba
Luke Andrews <luke@attaboy.ca>
2016-12-08 14:49:09 -0500

    Alter trigger sorting to prioritize non-regex triggers without params before those with params

--- app/json/BehaviorTriggerData.scala
+++ app/json/BehaviorTriggerData.scala
@@ -7,23 +7,29 @@ case class BehaviorTriggerData(
                                 caseSensitive: Boolean
                                 ) extends Ordered[BehaviorTriggerData] {
 
-  def sortString: String = {
-    if ("""^[A-Za-z0-9]""".r.findFirstMatchIn(this.text).isDefined) {
-      this.text
+  private def beginsWithAlphanumeric = {
+    """^[A-Za-z0-9]""".r.findFirstMatchIn(this.text).isDefined
+  }
+
+  private def containsNonRegexParam = {
+    """\{.+\}""".r.findFirstMatchIn(this.text).isDefined
+  }
+
+  private def sortString: String = {
+    if (this.isRegex) {
+      "3 " ++ this.text
+    } else if (this.beginsWithAlphanumeric) {
+      if (this.containsNonRegexParam) {
+        "1 " ++ this.text
+      } else {
+        "0 " ++ this.text
+      }
     } else {
-      "~" ++ this.text
+      "2 " ++ this.text
     }
   }
 
   def compare(that: BehaviorTriggerData): Int = {
-    if (this.isRegex && !that.isRegex) {
-      1
-    } else if (!this.isRegex && that.isRegex) {
-      -1
-    } else if (!this.isRegex && !that.isRegex) {
-      this.sortString compare that.sortString
-    } else {
-      this.text compare that.text
-    }
+    this.sortString compare that.sortString
   }
 }
--- test/BehaviorTriggerDataSpec.scala
+++ test/BehaviorTriggerDataSpec.scala
@@ -20,5 +20,22 @@ class BehaviorTriggerDataSpec extends PlaySpec {
       val trigger3 = triggerFor("a is for apple", isRegex = false)
       Array(trigger1, trigger2, trigger3).sorted mustBe Array(trigger3, trigger2, trigger1)
     }
+
+    "sort non-regex triggers with params after non-regex triggers without params" in {
+      val trigger1 = triggerFor("a {fruit}", isRegex = false)
+      val trigger2 = triggerFor("be fruit", isRegex = false)
+      Array(trigger1, trigger2).sorted mustBe Array(trigger2, trigger1)
+    }
+
+    "sort all permutations of triggers correctly" in {
+      val regex1 = triggerFor("abc", isRegex = true)
+      val regex2 = triggerFor(".+", isRegex = true)
+      val nonRegexNonAlphanumeric = triggerFor(":tada:", isRegex = false)
+      val nonRegexParams = triggerFor("yyy {zzz}", isRegex = false)
+      val nonRegexNoParams = triggerFor("zzzz", isRegex = false)
+      Array(regex1, regex2, nonRegexNonAlphanumeric, nonRegexParams, nonRegexNoParams).sorted mustBe {
+        Array(nonRegexNoParams, nonRegexParams, nonRegexNonAlphanumeric, regex2, regex1)
+      }
+    }
   }
 }
