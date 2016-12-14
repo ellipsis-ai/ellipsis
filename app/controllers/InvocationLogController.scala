cf06b247f81216501bbc7b759a2ba6761b4679eb
Andrew Catton <andrew@catton.ca>
2016-12-14 14:04:52 -0500

    Include a few more details in log entries returned, refactor

--- app/controllers/InvocationLogController.scala
+++ app/controllers/InvocationLogController.scala
@@ -2,12 +2,14 @@ package controllers
 
 import javax.inject.Inject
 
+import com.mohiva.play.silhouette.api.LoginInfo
 import org.joda.time.LocalDateTime
 import play.api.Configuration
 import play.api.i18n.MessagesApi
 import play.api.libs.json.{JsNull, JsValue, Json}
 import play.api.mvc.Action
 import json.Formatting._
+import models.behaviors.invocationlogentry.InvocationLogEntry
 import services.DataService
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -19,10 +21,35 @@ class InvocationLogController @Inject() (
                                  val dataService: DataService
                                ) extends EllipsisController {
 
-  case class ParamValues(values: JsValue, timestamp: LocalDateTime)
-  implicit val paramValuesWrites = Json.writes[ParamValues]
+  case class LogEntryData(
+                           paramValues: JsValue,
+                           context: String,
+                           userIdForContext: Option[String],
+                           ellipsisUserId: Option[String],
+                           timestamp: LocalDateTime
+                         )
 
-  def getParamValues(behaviorId: String, token: String) = Action.async { implicit request =>
+  object LogEntryData {
+    def forEntry(entry: InvocationLogEntry, dataService: DataService): Future[LogEntryData] = {
+      val eventualMaybeEllipsisUser = entry.maybeUserIdForContext.map { userIdForContext =>
+        dataService.linkedAccounts.find(LoginInfo(entry.context, userIdForContext), entry.behaviorVersion.team.id).map { maybeAcc =>
+          maybeAcc.map(_.user)
+        }
+      }.getOrElse(Future.successful(None))
+      eventualMaybeEllipsisUser.map { maybeUser =>
+        LogEntryData(
+          entry.paramValues,
+          entry.context,
+          entry.maybeUserIdForContext,
+          maybeUser.map(_.id),
+          entry.createdAt
+        )
+      }
+    }
+  }
+  implicit val logEntryWrites = Json.writes[LogEntryData]
+
+  def getLogs(behaviorId: String, token: String) = Action.async { implicit request =>
     for {
       maybeTeam <- dataService.teams.findForToken(token)
       maybeBehaviorWithoutAccessCheck <- dataService.behaviors.findWithoutAccessCheck(behaviorId)
@@ -34,12 +61,14 @@ class InvocationLogController @Inject() (
           Some(entries.filterNot(_.paramValues == JsNull))
         }
       }.getOrElse(Future.successful(None))
+      maybeLogEntryData <- maybeLogEntries.map { logEntries =>
+        Future.sequence(logEntries.map { ea =>
+          LogEntryData.forEntry(ea, dataService)
+        }).map(Some(_))
+      }.getOrElse(Future.successful(None))
     } yield {
-      maybeLogEntries.map { logEntries =>
-        val paramValues: Seq[ParamValues] = logEntries.map { ea =>
-          ParamValues(ea.paramValues, ea.createdAt)
-        }
-        Ok(Json.toJson(paramValues))
+      maybeLogEntryData.map { logEntryData =>
+        Ok(Json.toJson(logEntryData))
       }.getOrElse {
         NotFound("")
       }
