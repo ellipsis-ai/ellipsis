0b1055052963aa24e26d857dfb96c5fdd9a1e4d4
Andrew Catton <andrew@catton.ca>
2016-12-17 12:25:49 -0500

    Oops was missing a semicolon in down migration

--- conf/evolutions/default/61.sql
+++ conf/evolutions/default/61.sql
@@ -7,5 +7,5 @@ ALTER TABLE invocation_tokens DROP COLUMN team_id;
 # --- !Downs
 
 TRUNCATE TABLE invocation_tokens;
-ALTER TABLE invocation_tokens ADD COLUMN team_id TEXT NOT NULL REFERENCES teams(id)
+ALTER TABLE invocation_tokens ADD COLUMN team_id TEXT NOT NULL REFERENCES teams(id);
 ALTER TABLE invocation_tokens DROP COLUMN user_id;
