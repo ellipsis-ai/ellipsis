7ec0ab94860eda665d661236103d69be607fca4e
Luke Andrews <luke@attaboy.ca>
2017-01-10 23:25:12 -0500

    Add another blockquote test that includes other formatting

--- test/SlackMessageFormatterSpec.scala
+++ test/SlackMessageFormatterSpec.scala
@@ -9,6 +9,7 @@ class SlackMessageFormatterSpec extends PlaySpec {
     "handle blockquotes" in {
       escapeJava(SlackMessageFormatter.bodyTextFor(">foo")) mustBe """\r> foo \r> """
       escapeJava(SlackMessageFormatter.bodyTextFor(">foo\n>bar\n>\n>baz")) mustBe """\r> foo bar \r> \r> baz \r> """
+      escapeJava(SlackMessageFormatter.bodyTextFor(">_foo_ bar\n>\n>baz")) mustBe """\r> _foo_ bar \r> \r> baz \r> """
     }
 
   }
