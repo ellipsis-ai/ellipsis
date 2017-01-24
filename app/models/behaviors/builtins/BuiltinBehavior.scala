5d8a40d8d80be40eb1d6b56350f64c0708742fe9
Luke Andrews <luke@attaboy.ca>
2017-01-23 22:10:35 -0500

    "Uneducate" curly quotes before passing message text to built-in behaviors

--- app/models/behaviors/builtins/BuiltinBehavior.scala
+++ app/models/behaviors/builtins/BuiltinBehavior.scala
@@ -16,6 +16,10 @@ trait BuiltinBehavior {
 
 object BuiltinBehavior {
 
+  def uneducateQuotes(text: String): String = {
+    text.replaceAll("[“”]", "\"").replaceAll("[‘’]", "'")
+  }
+
   def maybeFrom(event: MessageEvent, lambdaService: AWSLambdaService, dataService: DataService): Option[BuiltinBehavior] = {
     val setEnvironmentVariableRegex = s"""(?i)(?s)^set\\s+env\\s+(\\S+)\\s+(.*)$$""".r
     val unsetEnvironmentVariableRegex = s"""(?i)^unset\\s+env\\s+(\\S+)\\s*$$""".r
@@ -24,13 +28,13 @@ object BuiltinBehavior {
     val helpRegex = s"""(?i)^help\\s*(\\S*.*)$$""".r
     val rememberRegex = s"""(?i)^(remember|\\^)\\s*$$""".r
     val scheduledRegex = s"""(?i)^scheduled$$""".r
-    val scheduleRegex = s"""(?i)^schedule\\s+[`"“'‘](.*?)[`"”'’](\\s+privately for everyone in this channel)?\\s+(.*)\\s*$$""".r
-    val unscheduleRegex = s"""(?i)^unschedule\\s+[`"“'‘](.*?)[`"”'’]\\s*$$""".r
+    val scheduleRegex = s"""(?i)^schedule\\s+[`"'](.*?)[`"'](\\s+privately for everyone in this channel)?\\s+(.*)\\s*$$""".r
+    val unscheduleRegex = s"""(?i)^unschedule\\s+[`"'](.*?)[`"']\\s*$$""".r
     val resetBehaviorsRegex = """(?i)reset behaviors really really really""".r
     val setTimeZoneRegex = s"""(?i)^set default time\\s*zone to\\s(.*)$$""".r
 
     if (event.includesBotMention) {
-      event.relevantMessageText match {
+      uneducateQuotes(event.relevantMessageText) match {
         case setEnvironmentVariableRegex(name, value) => Some(SetEnvironmentVariableBehavior(name, value, event, lambdaService, dataService))
         case unsetEnvironmentVariableRegex(name) => Some(UnsetEnvironmentVariableBehavior(name, event, lambdaService, dataService))
         case startLearnConversationRegex() => Some(LearnBehavior(event, lambdaService, dataService))
