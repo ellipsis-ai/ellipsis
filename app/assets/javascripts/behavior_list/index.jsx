d777b42736eac96fa7faabe943dc2a681a9f952f
Luke Andrews <luke@attaboy.ca>
2017-01-24 20:54:28 -0500

    Copy paste modal/panel stuff in so that we can use a confirmation panel for deleting skills on the main list

--- app/assets/javascripts/behavior_list/index.jsx
+++ app/assets/javascripts/behavior_list/index.jsx
@@ -3,8 +3,10 @@ define(function(require) {
     BehaviorName = require('./behavior_name'),
     BehaviorGroup = require('../models/behavior_group'),
     Collapsible = require('../collapsible'),
+    ConfirmActionPanel = require('../panels/confirm_action'),
     FixedFooter = require('../fixed_footer'),
     Formatter = require('../formatter'),
+    ModalScrim = require('../modal_scrim'),
     SVGInstalled = require('../svg/installed');
 
   return React.createClass({
@@ -30,7 +32,8 @@ define(function(require) {
 
     getInitialState: function() {
       return {
-        selectedGroupIds: []
+        selectedGroupIds: [],
+        activePanel: null
       };
     },
 
@@ -50,6 +53,36 @@ define(function(require) {
 
     isGroupSelected: function(groupId) {
       return this.getSelectedGroupIds().indexOf(groupId) >= 0;
+    },
+
+    toggleActivePanel: function(name, beModal, optionalCallback) {
+      var alreadyOpen = this.getActivePanelName() === name;
+      this.setState({
+        activePanel: alreadyOpen ? null : { name: name, isModal: !!beModal }
+      }, optionalCallback);
+    },
+
+    confirmDeleteBehaviorGroups: function() {
+      this.toggleActivePanel('confirmDeleteBehaviorGroups', true);
+    },
+
+    hideActivePanel: function() {
+      this.setState({
+        activePanel: null
+      });
+    },
+
+    getActivePanel: function() {
+      return this.state.activePanel;
+    },
+
+    getActivePanelName: function() {
+      return this.state.activePanel && this.state.activePanel.name ? this.state.activePanel.name : "";
+    },
+
+    activePanelIsModal: function() {
+      const panel = this.getActivePanel();
+      return !!(panel && panel.isModal);
     },
 
     onGroupSelectionCheckboxChange: function(groupId) {
@@ -139,6 +172,14 @@ define(function(require) {
         return "Delete skill";
       } else {
         return `Delete skills`;
+      }
+    },
+
+    getTextForDeleteBehaviorGroups: function(selectedCount) {
+      if (selectedCount === 1) {
+        return "Are you sure you want to delete this skill?";
+      } else {
+        return `Are you sure you want to delete ${selectedCount} skills?`;
       }
     },
 
@@ -244,7 +285,7 @@ define(function(require) {
           </button>
           <button type="button"
             className="mrs mbs"
-            onClick={this.deleteBehaviorGroups}
+            onClick={this.confirmDeleteBehaviorGroups}
             disabled={selectedCount < 1}
           >
             {this.getLabelForDeleteAction(selectedCount)}
@@ -293,11 +334,17 @@ define(function(require) {
             </div>
           </div>
 
-          <FixedFooter className="bg-white">
-            <Collapsible revealWhen={this.getSelectedGroupIds().length > 0}>
+          <ModalScrim isActive={this.activePanelIsModal()} onClick={this.hideActivePanel} />
+          <FixedFooter ref="footer" className="bg-white">
+            <Collapsible revealWhen={!this.getActivePanelName() && this.getSelectedGroupIds().length > 0}>
               <div className="container container-c ptm border-top">
                 {this.renderActions()}
               </div>
+            </Collapsible>
+            <Collapsible ref="confirmDeleteBehaviorGroups" revealWhen={this.getActivePanelName() === 'confirmDeleteBehaviorGroups'}>
+              <ConfirmActionPanel confirmText="Delete" onConfirmClick={this.deleteBehaviorGroups} onCancelClick={this.hideActivePanel}>
+                <p>{this.getTextForDeleteBehaviorGroups(this.getSelectedGroupIds().length)}</p>
+              </ConfirmActionPanel>
             </Collapsible>
           </FixedFooter>
         </div>
