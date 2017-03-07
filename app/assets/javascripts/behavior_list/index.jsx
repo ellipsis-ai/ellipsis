310ac7d602c7c4f954ca7d3a9ddf9233ff2c38fd
Luke Andrews <luke@attaboy.ca>
2017-03-07 13:29:55 -0500

    New tests for BehaviorList that test toggleInfoPanel's unusual logic

--- app/assets/javascripts/behavior_list/index.jsx
+++ app/assets/javascripts/behavior_list/index.jsx
@@ -1,17 +1,13 @@
 define(function(require) {
   var React = require('react'),
-    BehaviorName = require('./behavior_name'),
     BehaviorGroup = require('../models/behavior_group'),
     BehaviorGroupCard = require('./behavior_group_card'),
     BehaviorGroupInfoPanel = require('./behavior_group_info_panel'),
     Collapsible = require('../shared_ui/collapsible'),
     ConfirmActionPanel = require('../panels/confirm_action'),
     FixedFooter = require('../shared_ui/fixed_footer'),
-    Formatter = require('../lib/formatter'),
     ModalScrim = require('../shared_ui/modal_scrim'),
-    PageWithPanels = require('../shared_ui/page_with_panels'),
-    Sort = require('../lib/sort'),
-    SVGInstalled = require('../svg/installed');
+    PageWithPanels = require('../shared_ui/page_with_panels');
 
   const ANIMATION_DURATION = 0.25;
 
@@ -22,18 +18,12 @@ define(function(require) {
       csrfToken: React.PropTypes.string.isRequired
     }),
 
-    isGroupImported: function(group) {
-      return !!(group && group.exportId);
+    getAnimationDuration: function() {
+      return ANIMATION_DURATION;
     },
 
-    getImportedStatusFromGroupOrVersion: function(groupOrVersion) {
-      if (groupOrVersion.exportId) {
-        return (
-          <span title="Installed from ellipsis.ai" className="mls display-inline-block align-m" style={{ width: 30, height: 18 }}>
-            <SVGInstalled />
-          </span>
-        );
-      }
+    isGroupImported: function(group) {
+      return !!(group && group.exportId);
     },
 
     getBehaviorGroups: function() {
@@ -56,16 +46,6 @@ define(function(require) {
       }
     },
 
-    getTableRowClasses: function(index) {
-      if (index % 3 === 0) {
-        return " pts border-top ";
-      } else if (index % 3 === 2) {
-        return " pbs ";
-      } else {
-        return "";
-      }
-    },
-
     getSelectedGroupIds: function() {
       return this.state.selectedGroupIds || [];
     },
@@ -75,11 +55,11 @@ define(function(require) {
     },
 
     confirmDeleteBehaviorGroups: function() {
-      this.props.onToggleActivePanel('confirmDeleteBehaviorGroups', true);
+      this.toggleActivePanel('confirmDeleteBehaviorGroups', true);
     },
 
     confirmMergeBehaviorGroups: function() {
-      this.props.onToggleActivePanel('confirmMergeBehaviorGroups', true);
+      this.toggleActivePanel('confirmMergeBehaviorGroups', true);
     },
 
     onGroupSelectionCheckboxChange: function(groupId, isChecked) {
@@ -171,96 +151,28 @@ define(function(require) {
       return `Are you sure you want to merge these ${selectedCount} skills?`;
     },
 
-    groupHasTitle: function(group) {
-      return !!(group.name || group.description);
-    },
-
-    renderBehaviorVersionRow: function(version, versionIndex, group) {
-      var isFirstRow = versionIndex === 0 && !this.groupHasTitle(group);
-      var borderAndSpacingClass = isFirstRow ? "border-top pts " : "";
-      borderAndSpacingClass += (versionIndex === group.behaviorVersions.length - 1 ? "pbs " : "pbxs ");
-      return (
-        <div className="column-row" key={`version-${group.id}-${versionIndex}`}>
-          <div className={"column column-expand type-s type-wrap-words " + borderAndSpacingClass}>
-            <div className="columns columns-elastic">
-              <div className="column column-shrink prs">
-                {isFirstRow ? this.renderGroupSelectionCheckbox(group.id) : this.renderPlaceholderCheckbox()}
-              </div>
-              <div className="column column-expand">
-                <BehaviorName
-                  version={version}
-                  labelDataType={true}
-                />
-              </div>
-            </div>
-          </div>
-          <div className={"column column-shrink type-s type-weak display-ellipsis align-r mobile-display-none " + borderAndSpacingClass}>
-            {Formatter.formatTimestampRelativeIfRecent(version.createdAt)}
-            {this.getImportedStatusFromGroupOrVersion(version)}
-          </div>
-        </div>
-      );
-    },
-
-    renderBehaviorGroupTitle: function(optionalName, optionalDescription) {
-      if (optionalName && optionalDescription) {
-        return (
-          <h4 className="man">
-            <span>{optionalName}</span>
-            <span className="mhs type-weak">·</span>
-            <span className="type-regular">{optionalDescription}</span>
-          </h4>
-        );
-      } else if (optionalName) {
-        return (
-          <h4 className="man">{optionalName}</h4>
-        );
-      } else if (optionalDescription) {
-        return (
-          <h4 className="type-regular man">{optionalDescription}</h4>
-        );
-      } else {
-        return null;
-      }
-    },
-
-    renderBehaviorGroupTitleRow: function(group) {
-      if (this.groupHasTitle(group)) {
-        return (
-          <div className="column-row" key={`group-${group.id}-title`}>
-            <div className="column column-expand border-top pts pbxs">
-              <div className="columns columns-elastic">
-                <div className="column column-shrink prs">{this.renderGroupSelectionCheckbox(group.id)}</div>
-                <div className="column column-expand">{this.renderBehaviorGroupTitle(group.name, group.description)}</div>
-              </div>
-            </div>
-            <div className="column column-shrink border-top pts pbxs align-r mobile-display-none">
-              {this.getImportedStatusFromGroupOrVersion(group)}
-            </div>
-          </div>
-        );
-      } else {
-        return null;
-      }
-    },
-
-    renderBehaviorGroup: function(group) {
-      var sorted = Sort.arrayAlphabeticalBy(group.behaviorVersions, (version) => version.sortKey);
-      var versionRows = sorted.map((ea, i) => {
-        return this.renderBehaviorVersionRow(ea, i, group);
-      });
-      return [this.renderBehaviorGroupTitleRow(group)].concat(versionRows);
-    },
-
     getSelectedBehaviorGroup: function() {
       return this.state.selectedBehaviorGroup;
     },
 
+    getActivePanelName: function() {
+      return this.props.activePanelName;
+    },
+
+    clearActivePanel: function() {
+      this.props.onClearActivePanel();
+    },
+
+    toggleActivePanel: function(panelName, beModal) {
+      this.props.onToggleActivePanel(panelName, beModal);
+    },
+
     toggleInfoPanel: function(group) {
       var previousSelectedGroup = this.state.selectedBehaviorGroup;
-      if (group === previousSelectedGroup && this.props.activePanelName === 'moreInfo') {
-        this.props.onClearActivePanel();
-        return;
+      var panelOpen = this.getActivePanelName() === 'moreInfo';
+
+      if (panelOpen) {
+        this.clearActivePanel();
       }
 
       if (group && group !== previousSelectedGroup) {
@@ -268,17 +180,14 @@ define(function(require) {
           this.setState({
             selectedBehaviorGroup: group
           }, () => {
-            this.props.onToggleActivePanel('moreInfo');
+            this.toggleActivePanel('moreInfo');
           });
         };
-        if (this.props.activePanelName === 'moreInfo') {
-          this.props.onClearActivePanel();
-          window.setTimeout(openNewGroup, ANIMATION_DURATION * 1000);
+        if (panelOpen) {
+          window.setTimeout(openNewGroup, this.getAnimationDuration() * 1000);
         } else {
           openNewGroup();
         }
-      } else {
-        this.props.onToggleActivePanel('moreInfo');
       }
     },
 
@@ -366,12 +275,12 @@ define(function(require) {
             </div>
           </div>
 
-          <ModalScrim isActive={this.props.activePanelIsModal} onClick={this.props.onClearActivePanel} />
+          <ModalScrim isActive={this.props.activePanelIsModal} onClick={this.clearActivePanel} />
           <FixedFooter ref="footer" className="bg-white">
             <Collapsible
               ref="moreInfo"
-              revealWhen={this.props.activePanelName === 'moreInfo'}
-              animationDuration={ANIMATION_DURATION}
+              revealWhen={this.getActivePanelName() === 'moreInfo'}
+              animationDuration={this.getAnimationDuration()}
               onChange={this.resetFooterHeight}
             >
               <BehaviorGroupInfoPanel
@@ -382,7 +291,7 @@ define(function(require) {
               />
             </Collapsible>
             <Collapsible
-              revealWhen={!this.props.activePanelName && this.getSelectedGroupIds().length > 0}
+              revealWhen={!this.getActivePanelName() && this.getSelectedGroupIds().length > 0}
               onChange={this.resetFooterHeight}
             >
               <div className="container container-c ptm border-top">
@@ -390,28 +299,28 @@ define(function(require) {
               </div>
             </Collapsible>
             <Collapsible ref="confirmDeleteBehaviorGroups"
-              revealWhen={this.props.activePanelName === 'confirmDeleteBehaviorGroups'}
+              revealWhen={this.getActivePanelName() === 'confirmDeleteBehaviorGroups'}
               onChange={this.resetFooterHeight}
             >
               <ConfirmActionPanel
                 confirmText="Delete"
                 confirmingText="Deleting"
                 onConfirmClick={this.deleteBehaviorGroups}
-                onCancelClick={this.props.onClearActivePanel}
+                onCancelClick={this.clearActivePanel}
                 isConfirming={this.state.isSubmitting}
               >
                 <p>{this.getTextForDeleteBehaviorGroups(this.getSelectedGroupIds().length)}</p>
               </ConfirmActionPanel>
             </Collapsible>
             <Collapsible ref="confirmMergeBehaviorGroups"
-              revealWhen={this.props.activePanelName === 'confirmMergeBehaviorGroups'}
+              revealWhen={this.getActivePanelName() === 'confirmMergeBehaviorGroups'}
               onChange={this.resetFooterHeight}
             >
               <ConfirmActionPanel
                 confirmText="Merge"
                 confirmingText="Merging"
                 onConfirmClick={this.mergeBehaviorGroups}
-                onCancelClick={this.props.onClearActivePanel}
+                onCancelClick={this.clearActivePanel}
                 isConfirming={this.state.isSubmitting}
               >
                 <p>{this.getTextForMergeBehaviorGroups(this.getSelectedGroupIds().length)}</p>
