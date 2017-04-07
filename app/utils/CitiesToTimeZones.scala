dfbf3f2d2fd6f8df144c22594aed71d6cf83c6ca
Andrew Catton <andrew@catton.ca>
2017-04-06 21:37:25 -0400

    Load admin (if any) and country into CityInfo

--- app/utils/CitiesToTimeZones.scala
+++ app/utils/CitiesToTimeZones.scala
@@ -2,18 +2,44 @@ package utils
 
 import scala.io.Source
 
-case class CityInfo(name: String, asciiName: String, timeZoneId: String, population: Long)
+case class CityInfo(name: String, asciiName: String, admin: Option[String], country: String, timeZoneId: String, population: Long)
 
 class CitiesToTimeZones {
 
   val trie: Trie = Trie()
   val infoMap = scala.collection.mutable.Map[String, Set[CityInfo]]()
 
-  val dataStream = getClass.getResourceAsStream("data/cities15000.txt")
-  val cityNameColumnIndex = 1
-  val asciiNameColumnIndex = 2
-  val timeZoneColumnIndex = 17
-  val populationColumnIndex = 14
+  def loadAdmins: Map[String, String] = {
+    val adminCodeColumnIndex = 0
+    val adminNameColumnIndex = 1
+    val dataStream = getClass.getResourceAsStream("data/admin1CodesASCII.txt")
+    val bufferedSource = Source.fromInputStream(dataStream)
+    val map = scala.collection.mutable.Map[String, String]()
+    for (line <- bufferedSource.getLines) {
+      val cols = line.split("\t").map(_.trim)
+      val code = cols(adminCodeColumnIndex)
+      val name = cols(adminNameColumnIndex)
+      map.put(code, name)
+    }
+    bufferedSource.close
+    map.toMap
+  }
+
+  def loadCountries: Map[String, String] = {
+    val countryCodeColumnIndex = 0
+    val countryNameColumnIndex = 4
+    val dataStream = getClass.getResourceAsStream("data/countryInfo.txt")
+    val bufferedSource = Source.fromInputStream(dataStream)
+    val map = scala.collection.mutable.Map[String, String]()
+    for (line <- bufferedSource.getLines) {
+      val cols = line.split("\t").map(_.trim)
+      val code = cols(countryCodeColumnIndex)
+      val name = cols(countryNameColumnIndex)
+      map.put(code, name)
+    }
+    bufferedSource.close
+    map.toMap
+  }
 
   def addToInfoMap(key: String, info: CityInfo) = {
     trie.append(key)
@@ -22,18 +48,35 @@ class CitiesToTimeZones {
   }
 
   def load(): Unit = {
+    val cityNameColumnIndex = 1
+    val asciiNameColumnIndex = 2
+    val countryCodeColumnIndex = 8
+    val admin1CodeColumnIndex = 10
+    val timeZoneColumnIndex = 17
+    val populationColumnIndex = 14
+
+    val admins = loadAdmins
+    val countries = loadCountries
+
+    val dataStream = getClass.getResourceAsStream("data/cities15000.txt")
     val bufferedSource = Source.fromInputStream(dataStream)
     for (line <- bufferedSource.getLines) {
       val cols = line.split("\t").map(_.trim)
+
       val name = cols(cityNameColumnIndex)
       val asciiName = cols(asciiNameColumnIndex)
+      val countryCode = cols(countryCodeColumnIndex)
+      val country = countries(countryCode)
+      val admin1Code = cols(admin1CodeColumnIndex)
+      val admin1 = admins.get(s"$countryCode.$admin1Code")
       val tz = cols(timeZoneColumnIndex)
       val population: Long = try {
         cols(populationColumnIndex).toLong
       } catch {
         case e: NumberFormatException => 15000 // membership in the list means at least this many people
       }
-      val info = CityInfo(name, asciiName, tz, population)
+
+      val info = CityInfo(name, asciiName, admin1, country, tz, population)
 
       addToInfoMap(name, info)
       addToInfoMap(asciiName, info)
