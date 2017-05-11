9bd861b5ec758ad6104bc6d3f7272ed80defc709
Luke Andrews <luke@attaboy.ca>
2017-05-10 20:44:59 -0400

    Separate development filters from production filters so we can add CORS filtering for development only

--- app/filters/ProductionFilters.scala
+++ app/filters/ProductionFilters.scala
@@ -5,15 +5,13 @@ import javax.inject.Inject
 import play.api.http.HttpFilters
 import play.filters.gzip.GzipFilter
 import play.filters.headers.SecurityHeadersFilter
-import play.filters.cors.CORSFilter
 
-class Filters @Inject() (
+class ProductionFilters @Inject() (
                           https: HTTPSRedirectFilter,
                           csrfFilter: ExcludingCSRFFilter,
                           gzipFilter: GzipFilter,
-                          securityHeadersFilter: SecurityHeadersFilter,
-                          corsFilter: CORSFilter
+                          securityHeadersFilter: SecurityHeadersFilter
                           ) extends HttpFilters {
 
-  val filters = Seq(https, csrfFilter, gzipFilter, securityHeadersFilter, corsFilter)
+  val filters = Seq(https, csrfFilter, gzipFilter, securityHeadersFilter)
 }
