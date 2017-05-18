fd2245ba50348b7a49efb87e3666066f4d93892d
Luke Andrews <luke@attaboy.ca>
2017-05-18 10:09:38 -0400

    Revert "Maybe the config call needs to be synchronous?"
    This reverts commit 097a71f943f648f139608d49f6c81e82b9c1f1fe.

--- app/assets/javascripts/styleguide/colors/loader.jsx
+++ app/assets/javascripts/styleguide/colors/loader.jsx
@@ -1,13 +1,11 @@
 requirejs(['../../common'], function() {
-  requirejs(['config/styleguide/colors'], function(ColorsConfig) {
-    requirejs(
-      ['core-js', 'whatwg-fetch', 'react', 'react-dom', './styleguide/colors/index'],
-      function(Core, Fetch, React, ReactDOM, Colors) {
-        ReactDOM.render(
-          React.createElement(Colors, ColorsConfig),
-          document.getElementById(ColorsConfig.containerId)
-        );
-      }
-    );
-  });
+  requirejs(
+    ['core-js', 'whatwg-fetch', 'react', 'react-dom', './styleguide/colors/index', 'config/styleguide/colors'],
+    function(Core, Fetch, React, ReactDOM, Colors, ColorsConfig) {
+      ReactDOM.render(
+        React.createElement(Colors, ColorsConfig),
+        document.getElementById(ColorsConfig.containerId)
+      );
+    }
+  );
 });
