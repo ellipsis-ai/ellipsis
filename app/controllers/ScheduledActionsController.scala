f37eaaa88d43eb3f38c41dd611fe8c23e55e2400
Luke Andrews <luke@attaboy.ca>
2017-05-30 11:39:44 -0400

    Started to work on getting full scheduling info into a page that we can make a UI with

---
+++ app/controllers/ScheduledActionsController.scala
@@ -0,0 +1,48 @@
+package controllers
+
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.Silhouette
+import json.Formatting._
+import json._
+import models.silhouette.EllipsisEnv
+import play.api.Configuration
+import play.api.i18n.MessagesApi
+import play.api.libs.json.Json
+import services.DataService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+class ScheduledActionsController @Inject() (
+                                             val messagesApi: MessagesApi,
+                                             val configuration: Configuration,
+                                             val silhouette: Silhouette[EllipsisEnv],
+                                             val dataService: DataService
+                                            ) extends ReAuthable {
+
+  def index(maybeTeamId: Option[String]) = silhouette.SecuredAction.async { implicit request =>
+    val user = request.identity
+    for {
+      teamAccess <- dataService.users.teamAccessFor(user, maybeTeamId)
+      scheduledMessages <- teamAccess.maybeTargetTeam.map { team =>
+        dataService.scheduledMessages.allForTeam(team)
+      }.getOrElse(Future.successful(Seq()))
+      scheduledBehaviors <- teamAccess.maybeTargetTeam.map { team =>
+        dataService.scheduledBehaviors.allForTeam(team)
+      }.getOrElse(Future.successful(Seq()))
+      result <- teamAccess.maybeTargetTeam.map { team =>
+        ScheduledActionsData.fromScheduleData(team.id, dataService, scheduledMessages, scheduledBehaviors).map { data =>
+          val scheduledActionsJson = Json.toJson(data)
+          Ok(views.html.teams.settings.scheduled_actions.index(
+            viewConfig(Some(teamAccess)),
+            Json.prettyPrint(scheduledActionsJson)
+          ))
+        }
+      }.getOrElse {
+        Future.successful(NotFound("Team not accessible"))
+      }
+    } yield result
+  }
+}
--- app/json/ScheduledActionData.scala
+++ app/json/ScheduledActionData.scala
@@ -1,8 +1,43 @@
 package json
 
-case class ScheduledActionData(text: String)
+import models.behaviors.scheduling.scheduledbehavior.ScheduledBehavior
+import models.behaviors.scheduling.scheduledmessage.ScheduledMessage
+import services.DataService
 
-object ScheduledActionData {
-}
+import scala.concurrent.{ExecutionContext, Future}
+
+case class ScheduledActionArgumentData(name: String, value: String)
+
+case class ScheduledActionData(
+  actionName: Option[String],
+  trigger: Option[String],
+  arguments: Seq[ScheduledActionArgumentData],
+  recurrenceString: String,
+  useDM: Boolean,
+  channel: Option[String]
+)
 
 case class ScheduledActionsData(teamId: String, scheduledActions: Seq[ScheduledActionData])
+
+object ScheduledActionsData {
+  def fromScheduleData(teamId: String, dataService: DataService, scheduledMessages: Seq[ScheduledMessage], scheduledBehaviors: Seq[ScheduledBehavior])(implicit ec: ExecutionContext): Future[ScheduledActionsData] = {
+
+    val fromMessages = Future.sequence(scheduledMessages.map { ea =>
+      Future(ScheduledActionData(None, Some(ea.text), Seq(), ea.recurrence.displayString, ea.isForIndividualMembers, ea.maybeChannel))
+    })
+
+    val fromBehaviors = Future.sequence(scheduledBehaviors.map { ea =>
+      ea.displayText(dataService).map { name =>
+        val arguments = ea.arguments.map { case(key, value) => ScheduledActionArgumentData(key, value) }.toSeq
+        ScheduledActionData(Some(name), None, arguments, ea.recurrence.displayString, ea.isForIndividualMembers, ea.maybeChannel)
+      }
+    })
+
+    for {
+      messages <- fromMessages
+      behaviors <- fromBehaviors
+    } yield {
+      ScheduledActionsData(teamId, messages ++ behaviors)
+    }
+  }
+}
