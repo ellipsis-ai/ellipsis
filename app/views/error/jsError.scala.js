bb6dbbc669aa1d525391abbf74aa0f42a1fe339a
Luke Andrews <luke@attaboy.ca>
2017-05-31 13:03:01 -0400

    Add a stack trace to the console log as well

--- app/views/error/jsError.scala.js
+++ app/views/error/jsError.scala.js
@@ -5,5 +5,6 @@
 @shared.requireJsConfig()
 
 console.log("@{exception.title}: @{exception.description}");
-document.write("<pre class='pal'>@{exception.title}: @{exception.description}\n</pre>");
-document.write("<pre class='pal type-s'>Stack trace:\n@{exception.cause.getStackTrace.map(line => s"${line}\n")}</pre>");
+console.log("Stack trace:", @{JavaScript(exception.cause.getStackTrace.mkString("[\"", "\",\"", "\"]"))});
+document.write("<pre class='pal type-bold type-l'>@{exception.title}: @{exception.description}\n</pre>");
+document.write("<pre class='pal type-s'>Stack trace:\n@{exception.cause.getStackTrace.mkString("\n")}</pre>");
