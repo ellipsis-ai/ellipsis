68ae97887fa33694554e59ff8f6c3253ca551b99
Luke Andrews <luke@attaboy.ca>
2017-06-07 16:55:41 -0400

    DayOfMonth model class

--- app/assets/javascripts/form/day_of_month_input.jsx
+++ app/assets/javascripts/form/day_of_month_input.jsx
@@ -1,7 +1,7 @@
 define(function(require) {
   var React = require('react'),
     FormInput = require('./input'),
-    OptionalInt = require('../models/optional_int');
+    DayOfMonth = require('../models/day_of_month');
 
   return React.createClass({
     displayName: 'DayOfMonthInput',
@@ -11,31 +11,15 @@ define(function(require) {
     },
 
     onChange: function(newValue) {
-      const parsed = newValue.substr(-2, 2).match(/(3[0-1]|[1-2][0-9]|[1-9])$/);
-      this.props.onChange(OptionalInt.fromString(parsed ? parsed[1] : "").value);
+      this.props.onChange(DayOfMonth.fromString(newValue).value);
     },
 
     getTextValue: function() {
-      const day = this.props.value;
-      if (Number.isInteger(day)) {
-        return day.toString();
-      } else {
-        return "";
-      }
+      return new DayOfMonth(this.props.value).toString();
     },
 
     getOrdinalSuffix: function() {
-      const lastDigit = this.props.value % 10;
-      const last2Digits = this.props.value % 100;
-      if (lastDigit === 1 && last2Digits !== 11) {
-        return "st";
-      } else if (lastDigit === 2 && last2Digits !== 12) {
-        return "nd";
-      } else if (lastDigit === 3 && last2Digits !== 13) {
-        return "rd";
-      } else {
-        return "th";
-      }
+      return new DayOfMonth(this.props.value).ordinalSuffix();
     },
 
     render: function() {
