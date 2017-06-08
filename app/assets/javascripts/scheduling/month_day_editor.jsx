6df060e94855532648e6bad0e2ebbbd7019f3a38
Luke Andrews <luke@attaboy.ca>
2017-06-07 22:37:13 -0400

    Move month info into a Month type

--- app/assets/javascripts/scheduling/month_day_editor.jsx
+++ app/assets/javascripts/scheduling/month_day_editor.jsx
@@ -2,11 +2,8 @@ define(function(require) {
   var React = require('react'),
     DayOfMonthInput = require('../form/day_of_month_input'),
     Select = require('../form/select'),
+    Month = require('../models/month'),
     Recurrence = require('../models/recurrence');
-
-  const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const MONTH_ENUM_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const MAX_DAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   return React.createClass({
     displayName: 'MonthDayEditor',
@@ -24,39 +21,21 @@ define(function(require) {
     },
 
     getMonthText: function() {
-      return Number.isInteger(this.props.recurrence.month) ? this.props.recurrence.month.toString() : "";
-    },
-
-    getMaxDayForMonth: function(monthEnumValue) {
-      return MAX_DAYS[monthEnumValue - 1] || 31;
-    },
-
-    limitMaxDayForMonth: function(newMonth, newDay) {
-      if (Number.isInteger(newDay) && Number.isInteger(newMonth)) {
-        return Math.min(newDay, this.getMaxDayForMonth(newMonth));
-      } else {
-        return newDay;
-      }
+      return new Month(this.props.recurrence.month).toString();
     },
 
     onChangeDay: function(newDay) {
       this.props.onChange(this.props.recurrence.clone({
-        dayOfMonth: this.limitMaxDayForMonth(this.getMonth(), newDay)
+        dayOfMonth: new Month(this.getMonth()).limitDaytoMax(newDay)
       }));
     },
 
     onChangeMonth: function(newMonthText) {
-      const newMonth = parseInt(newMonthText, 10);
-      if (Number.isInteger(newMonth)) {
-        this.props.onChange(this.props.recurrence.clone({
-          month: newMonth,
-          dayOfMonth: this.limitMaxDayForMonth(newMonth, this.getDay())
-        }));
-      } else {
-        this.props.onChange(this.props.recurrence.clone({
-          month: null
-        }));
-      }
+      const newMonth = Month.fromString(newMonthText);
+      this.props.onChange(this.props.recurrence.clone({
+        month: newMonth.value,
+        dayOfMonth: newMonth.limitDaytoMax(this.getDay())
+      }));
     },
 
     render: function() {
@@ -69,9 +48,8 @@ define(function(require) {
           <span className="align-button mrm">of</span>
           <div className="align-button height-xl">
             <Select className="form-select-s" value={this.getMonthText()} onChange={this.onChangeMonth}>
-              <option value="" />
-              {MONTH_NAMES.map((name, index) => (
-                <option key={name} value={MONTH_ENUM_VALUES[index].toString()}>{name}</option>
+              {Month.YEAR.map((month) => (
+                <option key={month.name()} value={month.toString()}>{month.name()}</option>
               ))}
             </Select>
           </div>
