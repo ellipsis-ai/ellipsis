0ac3477b6ba7fa17c8cbcd2fe0f9811e7ebadf3e
Andrew Catton <andrew@catton.ca>
2017-07-17 16:35:52 -0400

    Clean up old lambda functions now that redeploy is done

--- app/services/AWSLambdaServiceImpl.scala
+++ app/services/AWSLambdaServiceImpl.scala
@@ -73,9 +73,9 @@ class AWSLambdaServiceImpl @Inject() (
   def listBehaviorFunctionNames: Future[Seq[String]] = {
     fetchFunctions(None).map { functions =>
       val allFunctionNames = functions.map(_.getFunctionName)
-      // TODO: clean up the old functions once we decide everything has worked
       val behaviorFunctionNames = allFunctionNames.filter { ea =>
-        ea.startsWith(BehaviorVersion.lambdaFunctionPrefix)
+        // TODO: put filter back in once old lambda functions are cleaned up
+        true || ea.startsWith(BehaviorVersion.lambdaFunctionPrefix)
       }
       behaviorFunctionNames
     }
