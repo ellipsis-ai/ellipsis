a2c6846c187bd7b1e39b982346320a05bd1738a8
Luke Andrews <luke@attaboy.ca>
2017-07-17 16:20:55 -0400

    Tweak the autobind spec

--- test/autobind_spec.js
+++ test/autobind_spec.js
@@ -4,6 +4,7 @@ describe("autobind", () => {
   class Foo {
     constructor() {
       this.prop = "set";
+      autobind(this);
     }
     foo() {
       return this.bar();
@@ -17,9 +18,9 @@ describe("autobind", () => {
   }
   it("sets the context of methods of an object to the object except React lifecycle methods and the constructor", () => {
     const f = new Foo();
-    autobind(f);
     const g = f.foo;
     expect(g()).toEqual("success");
+    expect(f.prop).toEqual("set");
     expect(f.foo).not.toBe(Foo.prototype.foo);
     expect(f.constructor).toBe(Foo.prototype.constructor);
     expect(f.render).toBe(Foo.prototype.render);
