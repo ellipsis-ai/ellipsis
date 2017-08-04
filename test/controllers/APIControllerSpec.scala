29afc1fa9870440295506995812832e59b84c843
Luke Andrews <luke@attaboy.ca>
2017-08-04 14:29:25 -0400

    Mock the user list method on SlackEventService

--- test/controllers/APIControllerSpec.scala
+++ test/controllers/APIControllerSpec.scala
@@ -73,11 +73,13 @@ class APIControllerSpec extends PlaySpec with MockitoSugar {
 
     val mockSlackClient = mock[SlackApiClient]
     when(slackEventService.clientFor(botProfile)).thenReturn(mockSlackClient)
+    when(slackEventService.maybeSlackUserListFor(botProfile)).thenReturn(Future.successful(Some(Seq())))
     when(mockSlackClient.listIms).thenReturn(Future.successful(Seq()))
     when(mockSlackClient.postChatMessage(anyString, anyString, any[Option[String]], any[Option[Boolean]], any[Option[String]],
       any[Option[String]], any[Option[Seq[Attachment]]], any[Option[Boolean]], any[Option[Boolean]],
       any[Option[String]], any[Option[String]], any[Option[Boolean]], any[Option[Boolean]],
       any[Option[String]], any[Option[Boolean]])(any[ActorSystem])).thenReturn(Future.successful(SlackTimestamp.now))
+    when(mockSlackClient.listUsers).thenReturn(Future.successful(Seq()))
 
     val event = SlackMessageEvent(botProfile, defaultChannel, None, defaultSlackUserId, "foo", SlackTimestamp.now, mockSlackClient, Seq())
     when(dataService.slackBotProfiles.allFor(team)).thenReturn(Future.successful(Seq(botProfile)))
