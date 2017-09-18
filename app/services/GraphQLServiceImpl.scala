17f5d95fe9fc23c502211301d0ad1513a897fb68
Luke Andrews <luke@attaboy.ca>
2017-09-18 12:47:27 -0400

    Upgrade sangria-play-json to instantiate JsBooleans properly, and add some code to coerce yes/no strings and 1/0 numbers into booleans

--- app/services/GraphQLServiceImpl.scala
+++ app/services/GraphQLServiceImpl.scala
@@ -6,6 +6,7 @@ import json.BehaviorGroupData
 import models.accounts.user.User
 import models.behaviors.behaviorgroup.BehaviorGroup
 import models.behaviors.behaviorgroupversion.BehaviorGroupVersion
+import models.behaviors.behaviorparameter.YesNoType
 import models.behaviors.datatypeconfig.BehaviorVersionForDataTypeSchema
 import models.behaviors.defaultstorageitem.DefaultStorageItemService
 import play.api.libs.json._
@@ -138,12 +139,41 @@ class GraphQLServiceImpl @Inject() (
               case arr: JsArray => fromJson(arr)
               case JsNull => null
               case _ => {
-                (ctx.value.asInstanceOf[JsObject] \ (definition.name)).asOpt[JsValue].map(fromJson)
+                val maybeValue = (ctx.value.asInstanceOf[JsObject] \ (definition.name)).asOpt[JsValue].map(fromJson)
+                if (definition.fieldType.namedType.name == "Boolean") {
+                  parseBoolean(maybeValue)
+                } else {
+                  maybeValue
+                }
               }
             }
           }
         }
       }
+    }
+
+    private def parseBoolean(maybeValue: Option[Any]) = {
+      maybeValue.map {
+        case b: Boolean => b
+        case s: String =>
+          val trimmed = s.toLowerCase.trim
+          if (YesNoType.yesStrings.contains(trimmed)) {
+            true
+          } else if (YesNoType.noStrings.contains(trimmed)) {
+            false
+          } else {
+            null
+          }
+        case d: Double =>
+          if (d == 1) {
+            true
+          } else if (d == 0) {
+            false
+          } else {
+            null
+          }
+        case _ => null
+      }.orNull
     }
 
     def fromJson(v: JsValue) = v match {
