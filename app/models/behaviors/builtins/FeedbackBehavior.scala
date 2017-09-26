c16a345acebe3ee9233c05bce1b4292afad26763
Luke Andrews <luke@attaboy.ca>
2017-09-26 14:53:37 -0400

    Remove unneeded services

--- app/models/behaviors/builtins/FeedbackBehavior.scala
+++ app/models/behaviors/builtins/FeedbackBehavior.scala
@@ -1,15 +1,13 @@
 package models.behaviors.builtins
 
 import akka.actor.ActorSystem
-import models.behaviors.{BotResult, SimpleTextResult}
 import models.behaviors.events.Event
-import services.{AWSLambdaService, DataService, DefaultServices}
+import models.behaviors.{BotResult, SimpleTextResult}
+import services.DefaultServices
 
 import scala.concurrent.{ExecutionContext, Future}
 
 case class FeedbackBehavior(event: Event, services: DefaultServices) extends BuiltinBehavior {
-  val dataService: DataService = services.dataService
-  val lambdaService: AWSLambdaService = services.lambdaService
 
   def result(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[BotResult] = {
     val msg = "Something to say? Say it to my face."
