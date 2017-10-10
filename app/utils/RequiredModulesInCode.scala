691d5f1a035c8cf5c24f288db09bf6bbe2fee0c0
Andrew Catton <andrew@catton.ca>
2017-10-10 16:42:10 -0400

    Rest of the changes

---
+++ app/utils/RequiredModulesInCode.scala
@@ -0,0 +1,37 @@
+package utils
+
+import models.behaviors.behaviorversion.BehaviorVersion
+import models.behaviors.library.LibraryVersion
+
+object RequiredModulesInCode {
+
+  val requireRegex = """.*require\s*\(['"]\s*(\S+)\s*['"]\).*""".r
+  val currentDirRegex = """^\.\/""".r
+
+  val alreadyIncludedModules = Array("aws-sdk", "dynamodb-doc")
+
+  def requiredModulesIn(code: String, libraries: Seq[LibraryVersion], includeLibraryRequires: Boolean): Seq[String] = {
+    val libraryNames = libraries.map(_.name)
+    val requiredForCode =
+      requireRegex.findAllMatchIn(code).
+        flatMap(_.subgroups.headOption).
+        map(_.trim).
+        map(ea => currentDirRegex.replaceAllIn(ea, "")).
+        toArray.
+        diff(alreadyIncludedModules ++ libraryNames).
+        sorted
+    val requiredForLibs = if (includeLibraryRequires) {
+      libraries.flatMap(ea => requiredModulesIn(ea.functionBody, libraries, includeLibraryRequires = false))
+    } else {
+      Seq()
+    }
+    (requiredForCode ++ requiredForLibs).distinct
+  }
+
+  def requiredModulesIn(behaviorVersions: Seq[BehaviorVersion], libraries: Seq[LibraryVersion], includeLibraryRequires: Boolean): Array[String] = {
+    behaviorVersions.flatMap { ea =>
+      requiredModulesIn(ea.functionBody, libraries, includeLibraryRequires)
+    }.distinct.toArray
+  }
+
+}
