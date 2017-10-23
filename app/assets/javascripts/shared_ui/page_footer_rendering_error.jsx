7f92955f30868105e676dd3ad9b9457b502b2015
Luke Andrews <luke@attaboy.ca>
2017-10-23 15:42:08 -0400

    Add a comment about why ES5 syntax for error subclass

--- app/assets/javascripts/shared_ui/page_footer_rendering_error.jsx
+++ app/assets/javascripts/shared_ui/page_footer_rendering_error.jsx
@@ -1,4 +1,5 @@
 define(function() {
+  /* Can't use ES6 class syntax because Babel doesn't extend built-in types properly */
   function PageFooterRenderingError(pageInstance) {
     const msg = `Page component’s onRenderFooter method not called by child component: ${
       pageInstance.component ? pageInstance.component.constructor.name : "(no child component)"
