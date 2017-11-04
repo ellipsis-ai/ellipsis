2ad6fe161966b5a480c1f8648331333ecc9fa544
Andrew Catton <andrew@catton.ca>
2017-11-04 13:06:30 -0400

    Add a missing route & small fix to fetch file js

--- conf/javascripts/lambda/ellipsis/fetch_function_for_file_param.js
+++ conf/javascripts/lambda/ellipsis/fetch_function_for_file_param.js
@@ -25,7 +25,7 @@ module.exports = function(param, $CONTEXT_PARAM) {
         } else if (err) {
           reject(err);
         } else {
-          reject(`$${res.statusCode}: $${body}`);
+          reject(`${res.statusCode}: ${body}`);
         }
       });
     });
