55681747b97849ecaa06bbded90c289ccf646e36
Andrew Catton <andrew@catton.ca>
2017-12-01 09:51:05 -0500

    Keep the original filename when using the thumbnail from Slack, for large images - Because Slack seems to not set the Content-Disposition header for the thumbnail image, we can instead do a HEAD request for the original file and get the Content-Disposition from that, maintaining the right filename

--- app/utils/SlackFileMap.scala
+++ app/utils/SlackFileMap.scala
@@ -4,6 +4,7 @@ import javax.inject.Inject
 
 import com.google.inject.Singleton
 import models.IDs
+import models.behaviors.events.SlackFile
 import services.CacheService
 
 @Singleton
@@ -12,16 +13,28 @@ class SlackFileMap @Inject() (
                               ) {
 
   private def keyFor(fileId: String): String = s"slack-file-map-$fileId"
+  private def thumbnailKeyFor(fileId: String): String = s"slack-file-map-thumbnail-$fileId"
 
-  def save(url: String): String = {
+  def save(file: SlackFile): String = {
     val fileId = IDs.next
-    cacheService.set(keyFor(fileId), url)
+    cacheService.set(keyFor(fileId), file.url)
+    file.maybeThumbnailUrl.foreach { thumbnailUrl =>
+      cacheService.set(thumbnailKeyFor(fileId), thumbnailUrl)
+    }
     fileId
   }
 
   def maybeUrlFor(fileId: String): Option[String] = {
     try {
       cacheService.get[String](keyFor(fileId))
+    } catch {
+      case e: IllegalArgumentException => None
+    }
+  }
+
+  def maybeThumbnailUrlFor(fileId: String): Option[String] = {
+    try {
+      cacheService.get[String](thumbnailKeyFor(fileId))
     } catch {
       case e: IllegalArgumentException => None
     }
