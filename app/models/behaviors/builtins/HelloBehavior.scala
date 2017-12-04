550307b3cc07a3e239ade65f8716048cacf9d6b8
Matteo <matteomelani@gmail.com>
2017-12-04 10:54:51 -0800

    Addresses code review comments

--- app/models/behaviors/builtins/HelloBehavior.scala
+++ app/models/behaviors/builtins/HelloBehavior.scala
@@ -8,16 +8,27 @@ import models.behaviors.{BotResult, SimpleTextResult}
 import services.DefaultServices
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class HelloBehavior(
                           event: Event,
                           services: DefaultServices
                         ) extends BuiltinBehavior {
 
+  private def version(services: DefaultServices): Future[Option[String]] = {
+    services.dataService.teams.allTeams.map { teams =>
+      services.configuration.getOptional[String]("application.version").map(Some(_)).getOrElse(None)
+    }
+  }
+
   def result(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[BotResult] = {
-    event.messageInfo(services).map { message =>
-      val userName: String = (message.details \ "name").as[String]
-      val reply = s"Hello @$userName"
+    for {
+      messageInfo <- event.messageInfo(services)
+      appVersion <- version(services)
+    } yield {
+      val greeting = (messageInfo.details \ "name").asOpt[String].map(name => (s"Hello @$name")).getOrElse("Hello")
+      val message = appVersion.map(v => (s"This is Ellipsis version $v")).getOrElse("This is Ellipis, I am ready to help.")
+      val reply = s"$greeting\n$message"
       SimpleTextResult(event, None, reply , forcePrivateResponse = true)
     }
   }
