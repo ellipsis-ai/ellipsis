fb0836aed5f9240058529bd7d2d337cd8d2d72d1
Luke Andrews <luke@attaboy.ca>
2017-12-12 11:43:42 -0500

    Make diff children in the version browser expandable, with default state collapsed

--- app/assets/javascripts/behavior_editor/versions/diff_item.jsx
+++ app/assets/javascripts/behavior_editor/versions/diff_item.jsx
@@ -1,23 +1,67 @@
 // @flow
 define(function(require) {
-  const React = require('react');
+  const React = require('react'),
+    Button = require('../../form/button'),
+    Collapsible = require('../../shared_ui/collapsible'),
+    SVGExpand = require('../../svg/expand'),
+    autobind = require('../../lib/autobind');
 
   type Props = {
     label: React.Node,
-    children: Array<React.Node>,
+    children?: React.Node,
     className?: string
   };
 
-  class DiffItem extends React.PureComponent<Props> {
+  type State = {
+    expanded: boolean
+  }
+
+  class DiffItem extends React.Component<Props, State> {
     props: Props;
+    state: State;
+
+    constructor(props) {
+      super(props);
+      autobind(this);
+      this.state = {
+        expanded: false
+      };
+    }
+
+    toggleExpanded(): void {
+      this.setState({
+        expanded: !this.state.expanded
+      });
+    }
+
+    renderChildren(): React.Node {
+      if (this.props.children) {
+        return (
+          <Collapsible revealWhen={this.state.expanded}>
+            {React.Children.map(this.props.children, (child) => (
+              <div className="mtxs border bg-lightest type-s">{child}</div>
+            ))}
+          </Collapsible>
+        );
+      }
+    }
 
     render(): React.Node {
       return (
         <div className={this.props.className || "pas border-bottom mbneg1"}>
-          <div>{this.props.label}</div>
-          {React.Children.map(this.props.children, (child) => (
-            <div className="mtxs border bg-lightest type-s">{child}</div>
-          ))}
+          <div>
+            {this.props.children ? (
+              <Button className="button-block" onClick={this.toggleExpanded}>
+                <span className="display-inline-block align-m height-xl mrs">
+                  <SVGExpand expanded={this.state.expanded} />
+                </span>
+                <span className="display-inline-block align-m">
+                  {this.props.label}
+                </span>
+              </Button>
+            ) : this.props.label}
+          </div>
+          {this.renderChildren()}
         </div>
       );
     }
