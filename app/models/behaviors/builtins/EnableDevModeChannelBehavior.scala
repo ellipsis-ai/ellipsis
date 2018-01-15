7a22aae7c3d6fdce30592b00ddedb5165c11a4f4
Andrew Catton <andrew@catton.ca>
2018-01-15 16:31:22 -0500

    Made it possible to enable/disable dev mode for a channel - this currently has no effect, but I'll use it soon with deployed skill versions - we can also use it to decide how much debugging info to display, or unlock dev builtins

---
+++ app/models/behaviors/builtins/EnableDevModeChannelBehavior.scala
@@ -0,0 +1,40 @@
+package models.behaviors.builtins
+
+import akka.actor.ActorSystem
+import models.behaviors.events.Event
+import models.behaviors.{BotResult, SimpleTextResult}
+import services.DefaultServices
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+case class EnableDevModeChannelBehavior(
+                                       event: Event,
+                                       services: DefaultServices
+                                     ) extends BuiltinBehavior {
+
+  def result(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[BotResult] = {
+    val dataService = services.dataService
+    val problemResponse = SimpleTextResult(event, None, "There was a problem trying to enable dev mode", forcePrivateResponse = false)
+    event.maybeChannel.map { channel =>
+      for {
+        maybeTeam <- dataService.teams.find(event.teamId)
+        maybeExisting <- maybeTeam.map { team =>
+          dataService.devModeChannels.find(event.context, channel, team)
+        }.getOrElse(Future.successful(None))
+        result <- maybeTeam.map { team =>
+          maybeExisting.map { _ =>
+            Future.successful(SimpleTextResult(event, None, s"This channel was already in dev mode, so I left it there", forcePrivateResponse = false))
+          }.getOrElse {
+            dataService.devModeChannels.ensureFor(event.context, channel, team).map { _ =>
+              SimpleTextResult(event, None, s"OK, this channel is now in dev mode", forcePrivateResponse = false)
+            }
+          }
+        }.getOrElse(Future.successful(problemResponse))
+      } yield result
+    }.getOrElse {
+      Future.successful(problemResponse)
+    }
+  }
+
+}
--- app/services/DataService.scala
+++ app/services/DataService.scala
@@ -39,6 +39,7 @@ import models.behaviors.scheduling.recurrence.RecurrenceService
 import models.behaviors.scheduling.scheduledbehavior.ScheduledBehaviorService
 import models.behaviors.scheduling.scheduledmessage.ScheduledMessageService
 import models.behaviors.triggers.messagetrigger.MessageTriggerService
+import models.devmodechannel.DevModeChannelService
 import models.environmentvariable.TeamEnvironmentVariableService
 import models.team.TeamService
 import slick.dbio.DBIO
@@ -87,6 +88,7 @@ trait DataService {
   val scheduledBehaviors: ScheduledBehaviorService
   val recurrences: RecurrenceService
   val invocationLogEntries: InvocationLogEntryService
+  val devModeChannels: DevModeChannelService
   def behaviorResponses: BehaviorResponseService
 
   def run[T](action: DBIO[T]): Future[T]
