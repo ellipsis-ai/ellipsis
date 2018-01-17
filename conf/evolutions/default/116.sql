6cda9523c2235fa3c55dbc2c81a92588cbbc597a
Andrew Catton <andrew@catton.ca>
2018-01-17 13:53:26 -0500

    Added migration to add a deployment for the most recent skill version

--- conf/evolutions/default/116.sql
+++ conf/evolutions/default/116.sql
@@ -1,5 +1,7 @@
 # --- !Ups
 
+BEGIN;
+
 CREATE TABLE behavior_group_deployments (
   id TEXT PRIMARY KEY,
   group_id TEXT NOT NULL REFERENCES behavior_groups(id) ON DELETE CASCADE,
@@ -14,11 +16,21 @@ CREATE INDEX behavior_group_deployments_group_id_index ON behavior_group_deploym
 CREATE INDEX behavior_group_deployments_group_version_id_index ON behavior_group_deployments(group_version_id);
 CREATE INDEX behavior_group_deployments_user_id_index ON behavior_group_deployments(user_id);
 
+INSERT INTO behavior_group_deployments
+SELECT DISTINCT ON (group_id) generate_pseudorandom_id(), group_id, id, NULL, author_id, created_at
+FROM behavior_group_versions
+ORDER BY group_id, created_at DESC;
+
+COMMIT;
 
 # --- !Downs
+
+BEGIN;
 
 DROP INDEX IF EXISTS behavior_group_deployments_group_id_index;
 DROP INDEX IF EXISTS behavior_group_deployments_group_version_id_index;
 DROP INDEX IF EXISTS behavior_group_deployments_user_id_index;
 
 DROP TABLE IF EXISTS behavior_group_deployments;
+
+COMMIT;
