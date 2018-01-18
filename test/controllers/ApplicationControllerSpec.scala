5f4f6a587591c9a8f4a39c950c1c3b78785c1fe6
Andrew Catton <andrew@catton.ca>
2018-01-18 11:33:41 -0500

    Updated one more test (was missing a mock)

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -59,6 +59,7 @@ class ApplicationControllerSpec extends PlaySpec with MockitoSugar {
         when(dataService.requiredSimpleTokenApis.allFor(behaviorGroupVersion)).thenReturn(Future.successful(Seq()))
         when(dataService.teamEnvironmentVariables.lookForInCode(anyString)).thenReturn(Seq())
         when(dataService.dataTypeConfigs.maybeFor(behaviorVersion)).thenReturn(Future.successful(None))
+        when(dataService.behaviorGroupDeployments.findForBehaviorGroupVersion(behaviorGroupVersion)).thenReturn(Future.successful(None))
         when(githubService.execute(anyString, any[JsValue])).thenReturn {
           Future.successful {
             Json.parse(
