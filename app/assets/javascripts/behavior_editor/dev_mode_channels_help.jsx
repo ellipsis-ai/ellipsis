36a79d0902318fb3aa942becce4cf919d828e63a
Andrew Catton <andrew@catton.ca>
2018-01-19 16:23:13 -0500

    Tweak line spacing in help

--- app/assets/javascripts/behavior_editor/dev_mode_channels_help.jsx
+++ app/assets/javascripts/behavior_editor/dev_mode_channels_help.jsx
@@ -17,17 +17,17 @@ define(function(require) {
             To let you work on a skill without interfering with its users, Ellipsis gives you the ability to only deploy a skill version when you are ready.
           </div>
 
-          <div className="mtm">
+          <div className="mtl">
             <span>You can test the latest version of a not-yet-deployed skill here in the editor using the <span className="type-bold">Test…</span> button below. </span>
           </div>
-          <div className="mtm">
+          <div className="mtl">
             <div>Additionally, you can enable "dev mode" in any channel in Slack by typing:</div>
-            <div className="box-code-example">…enable dev mode</div>
-            <div>In this mode, the most recently saved versions of your skills will be available, rather than the most recently deployed ones.</div>
+            <div className="box-code-example mts">…enable dev mode</div>
+            <div className="mts">In this mode, the most recently saved versions of your skills will be available, rather than the most recently deployed ones.</div>
           </div>
-          <div className="mtm">
+          <div className="mtl mbl">
             <span>When you are done, you can put the channel back to normal by typing:</span>
-            <div className="box-code-example">…disable dev mode</div>
+            <div className="box-code-example mts">…disable dev mode</div>
           </div>
 
         </HelpPanel>
