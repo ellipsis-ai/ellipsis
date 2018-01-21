d64675969798478ac1839fa847f9ace45c59f37c
Matteo <matteomelani@gmail.com>
2018-01-21 11:13:32 -0800

    small clean ups

--- scripts/billing/create_plans.py
+++ scripts/billing/create_plans.py
@@ -5,39 +5,40 @@ import chargebee
 plans = [
   {
     "id" : "developer-free-v1",
-    "name" : "Developer",
-    "invoice_name" : "Developer Free Plan",
+    "name" : "Developer(v1)",
+    "invoice_name" : "Developer Plan (v1)",
+    "Description" : "The best plan to get to know Ellipsis.",
     "price" : 0,
     "currency_code": "USD"
   },
   {
     "id" : "starter-v1",
-    "name" : "Starter",
-    "invoice_name" : "Stater Plan v1",
+    "name" : "Starter(v1)",
+    "invoice_name" : "Stater Plan (v1)",
     "Description" : "The best plan for small organizations. $5/month/active user.",
     "price" : 0,
     "currency_code": "USD"
   },
   {
     "id" : "business-v1",
-    "name" : "Business",
-    "invoice_name" : "Business Plan v1",
+    "name" : "Business(v1)",
+    "invoice_name" : "Business Plan (v1)",
     "Description" : "The best plan for organizations with 50+ people. $8/month/active user.",
     "price" : 0,
     "currency_code": "USD"
   },
   {
     "id" : "enterprise-v1",
-    "name" : "Enterprise",
-    "invoice_name" : "Enterprise Plan v1",
+    "name" : "Enterprise(v1)",
+    "invoice_name" : "Enterprise Plan (v1)",
     "Description" : "The best plan for Enterprise that need the maximum value.",
     "price" : 0,
     "currency_code": "USD"
   },
   {
     "id" : "expert-package-1-v1",
-    "name" : "Expert Package - 1 Skill",
-    "invoice_name" : "Expert Package - 1 Skill",
+    "name" : "Expert Package - 1 Skill(v1)",
+    "invoice_name" : "Expert Package - 1 Skill(v1)",
     "Description" : "One custom Skill with support for 12 months.",
     "period": 1,
     "period_unit": "year",
@@ -46,8 +47,8 @@ plans = [
   },
   {
     "id" : "expert-package-3-v1",
-    "name" : "Expert Package - 3 Skills",
-    "invoice_name" : "Expert Package - 3 Skills",
+    "name" : "Expert Package - 3 Skills(v1)",
+    "invoice_name" : "Expert Package - 3 Skills(v1)",
     "Description" : "3 custom Skill with support for 12 months.",
     "period": 1,
     "period_unit": "year",
@@ -56,9 +57,9 @@ plans = [
   },
   {
     "id" : "expert-package-enterprise-v1",
-    "name" : "Expert Package - Enterprise",
-    "invoice_name" : "Expert Package - Enterprise",
-    "Description" : "The ultimate package for custom Skills",
+    "name" : "Expert Package - Enterprise(v1)",
+    "invoice_name" : "Expert Package - Enterprise(v1)",
+    "Description" : "The ultimate package for custom Skills.",
     "period": 1,
     "period_unit": "year",
     "price" : 5000000,
