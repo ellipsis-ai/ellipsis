9c3cc368a637c703f8e67ddd684176d2917d9585
Andrew Catton <andrew@catton.ca>
2018-01-26 16:54:37 -0500

    Remove some obsolete code

--- app/json/ImmutableBehaviorGroupVersionData.scala
+++ app/json/ImmutableBehaviorGroupVersionData.scala
@@ -18,26 +18,3 @@ case class ImmutableBehaviorGroupVersionData(
                                               exportId: Option[String],
                                               createdAt: Option[OffsetDateTime]
                                             )
-
-object ImmutableBehaviorGroupVersionData {
-
-  def buildFor(groupVersionId: String, groupId: String, data: BehaviorGroupData): ImmutableBehaviorGroupVersionData = {
-    ImmutableBehaviorGroupVersionData(
-      groupVersionId,
-      groupId,
-      data.teamId,
-      data.author.map(_.id),
-      data.name,
-      data.description,
-      data.icon,
-      data.actionInputs,
-      data.dataTypeInputs,
-      data.behaviorVersions,
-      data.libraryVersions,
-      data.gitSHA,
-      data.exportId,
-      data.createdAt
-    )
-  }
-
-}
