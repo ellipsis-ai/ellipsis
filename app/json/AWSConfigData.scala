539fd77ccccf1019d1d64ec0794ffcb0850fe63c
Andrew Catton <andrew@catton.ca>
2018-01-26 11:46:04 -0500

    Remove unused and sensitive AWS config keys from JSON data being sent to the client.

--- app/json/AWSConfigData.scala
+++ app/json/AWSConfigData.scala
@@ -4,16 +4,13 @@ import models.behaviors.config.awsconfig.AWSConfig
 
 case class AWSConfigData(
                           id: String,
-                          displayName: String,
-                          accessKeyId: Option[String],
-                          secretAccessKey: Option[String],
-                          region: Option[String]
+                          displayName: String
                           )
 
 object AWSConfigData {
 
   def from(cfg: AWSConfig): AWSConfigData = {
-    AWSConfigData(cfg.id, cfg.name, cfg.maybeAccessKey, cfg.maybeSecretKey, cfg.maybeRegion)
+    AWSConfigData(cfg.id, cfg.name)
   }
 
 }
