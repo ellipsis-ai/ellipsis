61f8724bf5bb8fa20f5bef8f880607ebdbeb70d2
Luke Andrews <luke@attaboy.ca>
2018-02-12 16:00:59 -0500

    Add tests that make sure we preserve the existing JSON data in detailsFor for Slack messages so that we don't break any existing skills

--- app/models/behaviors/events/SlackEvent.scala
+++ app/models/behaviors/events/SlackEvent.scala
@@ -3,6 +3,7 @@ package models.behaviors.events
 import akka.actor.ActorSystem
 import com.mohiva.play.silhouette.api.LoginInfo
 import json.Formatting._
+import json.SlackUserData
 import models.accounts.slack.botprofile.SlackBotProfile
 import models.accounts.slack.profile.SlackProfile
 import play.api.libs.json._
@@ -38,6 +39,22 @@ trait SlackEvent {
     }
   }
 
+  private def legacyProfileDataFor(slackUserData: SlackUserData): JsObject = {
+    Json.obj(
+      "name" -> slackUserData.getDisplayName,
+      "profile" -> Json.obj(
+        "firstName" -> slackUserData.profile.profile.firstName,
+        "lastName" -> slackUserData.profile.profile.lastName,
+        "realName" -> slackUserData.profile.profile.realName
+      ),
+      "isPrimaryOwner" -> slackUserData.profile.isPrimaryOwner,
+      "isOwner" -> slackUserData.profile.isOwner,
+      "isRestricted" -> slackUserData.profile.isRestricted,
+      "isUltraRestricted" -> slackUserData.profile.isUltraRestricted,
+      "tz" -> slackUserData.profile.tz
+    )
+  }
+
   def detailsFor(services: DefaultServices)(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[JsObject] = {
     val slackChannels = SlackChannels(client, services.cacheService, profile.slackTeamId)
     for {
@@ -49,7 +66,7 @@ trait SlackEvent {
         "channelName" -> Json.toJson(maybeChannelInfo.map(_.name))
       ))
       maybeUser.map { user =>
-        Json.toJson(user.profile).as[JsObject] ++ channelDetails
+        Json.toJson(user.profile).as[JsObject] ++ legacyProfileDataFor(user) ++ channelDetails
       }.getOrElse {
         channelDetails
       }
