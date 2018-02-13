6788ada378667ad9806487995a17648ac26008df
matteomelani <matteomelani@gmail.com>
2018-02-13 06:56:09 -0800

    fixed gitingore

---
+++ test/tags/BillingTest.java
@@ -0,0 +1,10 @@
+package tags;
+
+import java.lang.annotation.*;
+import org.scalatest.TagAnnotation;
+
+
+@TagAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface BillingTest {}
