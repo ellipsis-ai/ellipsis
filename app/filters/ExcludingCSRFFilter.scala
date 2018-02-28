39b9be948cc427f8950bfc345ac3f99a5e3c29f8
Luke Andrews <luke@attaboy.ca>
2018-02-27 23:51:27 -0500

    Switch to Play's new recommended way of looking at route comments

--- app/filters/ExcludingCSRFFilter.scala
+++ app/filters/ExcludingCSRFFilter.scala
@@ -3,6 +3,7 @@ package filters
 import javax.inject.Inject
 
 import play.api.mvc.{EssentialFilter, EssentialAction}
+import play.api.routing.Router.Attrs
 import play.filters.csrf.CSRFFilter
 
 class ExcludingCSRFFilter @Inject() (csrfFilter: CSRFFilter) extends EssentialFilter {
@@ -11,7 +12,7 @@ class ExcludingCSRFFilter @Inject() (csrfFilter: CSRFFilter) extends EssentialFi
     import play.api.mvc._
 
     override def apply(rh: RequestHeader) = {
-      if (rh.tags.getOrElse("ROUTE_COMMENTS", "").contains("NOCSRF")) {
+      if (rh.attrs.get(Attrs.HandlerDef).exists(_.comments.contains("NOCSRF"))) {
         nextFilter(rh)
       } else {
         val chainedFilter = csrfFilter.apply(nextFilter)
