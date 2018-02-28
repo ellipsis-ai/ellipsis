a905a64ed9807f8c8fa2c8c0b18e2f1ca3e3f5e0
Luke Andrews <luke@attaboy.ca>
2018-02-28 16:25:57 -0500

    Convert skills list page/loader to Flow

--- flow-typed/jsRoutes.js
+++ flow-typed/jsRoutes.js
@@ -7,18 +7,24 @@ type JsRoute = {
 declare var jsRoutes: {
   controllers: {
     ApplicationController: {
+      deleteBehaviorGroups: () => JsRoute,
+      findBehaviorGroupsMatching: (queryString: string, branch: ?string, teamId: string) => JsRoute,
       index: (teamId?: string) => JsRoute,
-      setTeamTimeZone: () => JsRoute
+      setTeamTimeZone: () => JsRoute,
+      fetchPublishedBehaviorInfo: (teamId: string, branchName?: ?string) => JsRoute,
+      mergeBehaviorGroups: () => JsRoute
     },
     BehaviorEditorController: {
       deleteDefaultStorageItems: () => JsRoute,
       edit: (groupId: string, selectedId?: string, showVersions?: boolean) => JsRoute,
       pushToGithub: () => JsRoute,
       queryDefaultStorage: () => JsRoute,
+      save: () => JsRoute,
       saveDefaultStorageItem: () => JsRoute,
       updateFromGithub: () => JsRoute
     },
     BehaviorImportExportController: {
+      doImport: () => JsRoute,
       "export": (string) => JsRoute
     },
     GithubConfigController: {
