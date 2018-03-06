0daca3830713d78790381b3663c345f50b4bb881
Luke Andrews <luke@attaboy.ca>
2018-03-06 10:30:53 -0500

    Freeze DOCS_URLS

--- app/assets/frontend/lib/constants.jsx
+++ app/assets/frontend/lib/constants.jsx
@@ -1,10 +1,10 @@
 const NODE_JS_VERSION = "6.10.2";
 const NODE_JS_DOCS_URL = "https://nodejs.org/docs/latest-v6.x/api/";
 const GRAPHQL_DOCS_URL = "http://graphql.org/learn/";
-const DOCS_URLS = {
+const DOCS_URLS = Object.freeze({
   INPUTS: "https://docs.ellipsis.ai/developers/reference/02_actions/02_inputs/",
   TRIGGERS: "https://docs.ellipsis.ai/developers/reference/02_actions/01_triggers/"
-};
+});
 const MOBILE_MAX_WIDTH = 768;
 
 export {
