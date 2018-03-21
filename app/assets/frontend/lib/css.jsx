ddc99117eccc129d1d9d195c3d6ccac3c02cee2c
Luke Andrews <luke@attaboy.ca>
2018-03-20 20:51:02 -0400

    Remove CSS lib and inline its single use

--- app/assets/frontend/lib/css.jsx
+++
@@ -1,7 +0,0 @@
-const CSS = {
-    visibleWhen: function(condition) {
-      return " visibility " + (condition ? "visibility-visible" : "visibility-hidden") + " ";
-    }
-};
-
-export default CSS;
