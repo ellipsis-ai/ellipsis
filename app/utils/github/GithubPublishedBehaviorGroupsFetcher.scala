5ce4fa0458aea677696b479c4a61d23b7ba954ed
Andrew Catton <andrew@catton.ca>
2018-03-22 16:26:55 -0400

    Removed some unused vals

--- app/utils/github/GithubPublishedBehaviorGroupsFetcher.scala
+++ app/utils/github/GithubPublishedBehaviorGroupsFetcher.scala
@@ -2,7 +2,6 @@ package utils.github
 
 import json._
 import models.team.Team
-import play.api.Configuration
 import services.{DefaultServices, GithubService}
 
 import scala.concurrent.ExecutionContext
@@ -15,13 +14,6 @@ case class GithubPublishedBehaviorGroupsFetcher(
                                                   services: DefaultServices,
                                                   implicit val ec: ExecutionContext
                                                 ) {
-
-  val config: Configuration = services.configuration
-  val branch: String = maybeBranch.getOrElse("master")
-
-  val owner: String = "ellipsis-ai"
-  val repoName: String = "behaviors"
-  val token: String = config.get[String]("github.repoAccessToken")
 
   def result: Seq[BehaviorGroupData] = {
     val commits = GithubSkillCommitsFetcher(team, maybeBranch, alreadyInstalled, githubService, services, ec).result
