a2c4f18aa01231c2a350fb75dc4e8e2d680eeccf
Andrew Catton <andrew@catton.ca>
2018-04-04 11:11:50 -0400

    Don't drop the slack_profiles table in this change, because it might cause errors during a deploy

--- conf/evolutions/default/123.sql
+++ conf/evolutions/default/123.sql
@@ -5,23 +5,10 @@ BEGIN;
 ALTER TABLE linked_accounts
 DROP CONSTRAINT IF EXISTS linked_accounts_provider_id_provider_key_key;
 
-DROP TABLE IF EXISTS slack_profiles;
-
 ALTER TABLE conversations ADD COLUMN team_id_for_context TEXT;
 
 COMMIT;
 
 # --- !Downs
 
-BEGIN;
-
-CREATE TABLE slack_profiles (
-  provider_id TEXT NOT NULL,
-  provider_key TEXT NOT NULL,
-  team_id TEXT NOT NULL,
-  PRIMARY KEY(provider_id, provider_key)
-);
-
 ALTER TABLE conversations DROP COLUMN team_id_for_context;
-
-COMMIT;
