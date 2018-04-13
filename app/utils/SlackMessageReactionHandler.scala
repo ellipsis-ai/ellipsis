9391c19ad50dc395e0cd998cda23f292eaf1c308
Luke Andrews <luke@attaboy.ca>
2018-04-13 13:01:01 -0400

    Change the emoji reaction handler to send OK and alternating hourglass emojis to show progress when an action takes a long time

--- app/utils/SlackMessageReactionHandler.scala
+++ app/utils/SlackMessageReactionHandler.scala
@@ -7,6 +7,8 @@ import slack.api.SlackApiClient
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
 object SlackMessageReactionHandler {
+
+
   def handle[T](client: SlackApiClient, future: Future[T], channel: String, messageTs: String, delayMilliseconds: Int = 1500)
                (implicit system: ActorSystem): Future[Unit] = {
     implicit val ec: ExecutionContext = system.dispatcher
@@ -20,10 +22,30 @@ object SlackMessageReactionHandler {
     Future {
       Thread.sleep(delayMilliseconds)
       if (!p.isCompleted) {
-        client.addReactionToMessage("thinking_face", channel, messageTs).map { _ =>
-          p.future.onComplete(_ => client.removeReactionFromMessage("thinking_face", channel, messageTs))
-        }
+        client.addReactionToMessage("ok", channel, messageTs)
+        p.future.onComplete(_ => {
+          client.removeReactionFromMessage("ok", channel, messageTs)
+          client.removeReactionFromMessage("hourglass", channel, messageTs)
+          client.removeReactionFromMessage("hourglass_flowing_sand", channel, messageTs)
+        })
+        updateReactionClock(p, client, channel, messageTs)
       }
+    }
+  }
+
+  private def emojiFor(elapsedSeconds: Int): String = {
+    if (elapsedSeconds % 2 == 1) { "hourglass" } else { "hourglass_flowing_sand" }
+  }
+
+  private def updateReactionClock[T](p: Promise[T], client: SlackApiClient, channel: String, messageTs: String, elapsedSeconds: Int = 0)
+                            (implicit system: ActorSystem): Unit = {
+    implicit val ec: ExecutionContext = system.dispatcher
+    val next = elapsedSeconds + 1
+    client.removeReactionFromMessage(emojiFor(elapsedSeconds), channel, messageTs)
+    if (!p.isCompleted) {
+      client.addReactionToMessage(emojiFor(next), channel, messageTs)
+      Thread.sleep(3000)
+      updateReactionClock(p, client, channel, messageTs, next)
     }
   }
 }
