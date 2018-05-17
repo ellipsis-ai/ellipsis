d4d8733ab6e64fad70b21f64e847b3f69626c92a
Luke Andrews <luke@attaboy.ca>
2018-05-17 11:14:23 -0400

    Remove "Hello, " from the default response template, and move it into the example code instead

--- app/assets/frontend/models/behavior_version.tsx
+++ app/assets/frontend/models/behavior_version.tsx
@@ -370,9 +370,9 @@ class BehaviorVersion extends Editable implements Diffable, BehaviorVersionInter
 `// Write a Node.js (6.10.2) function that calls ellipsis.success() with a result.
 // You can require any NPM package.
 const name = ellipsis.userInfo.fullName || "friend";
-ellipsis.success(name);
+ellipsis.success(\`Hello, \${name}.\`);
 `);
-      const template = ResponseTemplate.fromString("Hello, {successResult}");
+      const template = ResponseTemplate.fromString("{successResult}");
       const triggers = [new Trigger(`run ${optionalName || "action"}`, false, true)];
       const props: DefaultActionProps = {
         triggers: triggers,
