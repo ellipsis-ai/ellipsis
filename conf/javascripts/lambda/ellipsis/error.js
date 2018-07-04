4a6ca17c5163683bc90877dd9aff2a89ae587232
Luke Andrews <luke@attaboy.ca>
2018-07-04 10:43:58 -0400

    Allow EllipsisError to be constructed from existing JS errors while keeping their contents

--- conf/javascripts/lambda/ellipsis/error.js
+++ conf/javascripts/lambda/ellipsis/error.js
@@ -1,6 +1,13 @@
 class EllipsisError extends Error {
-  constructor(systemMessage, options) {
-    super(systemMessage);
+  constructor(messageOrError, options) {
+    if (messageOrError instanceof Error) {
+      super(messageOrError.message);
+      Object.keys(messageOrError).map((key) => {
+        this[key] = messageOrError[key];
+      });
+    } else {
+      super(messageOrError);
+    }
     this.userMessage = options && options.userMessage ? options.userMessage : null;
   }
 }
