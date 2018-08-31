f6dd074fcf8cacc6885b6d7ccbd93d2b374c1d56
Luke Andrews <luke@attaboy.ca>
2018-08-31 12:31:13 -0400

    Remove unneeded import

--- test/assets/frontend/scheduling/recurrence_times_to_run_editor_spec.ts
+++ test/assets/frontend/scheduling/recurrence_times_to_run_editor_spec.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {RecurrenceEditorProps} from "../../../../app/assets/frontend/scheduling/recurrence_editor";
 import * as TestUtils from "react-addons-test-utils";
 import RecurrenceTimesToRunEditor from "../../../../app/assets/frontend/scheduling/recurrence_times_to_run_editor";
 import Recurrence, {RecurrenceInterface} from "../../../../app/assets/frontend/models/recurrence";
