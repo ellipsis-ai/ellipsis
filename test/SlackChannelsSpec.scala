45aeb8ba496e5f1a19ccf685222bba0c47651627
Luke Andrews <luke@attaboy.ca>
2018-09-11 00:37:24 -0400

    Fix the matcher for mocking conversation getInfo

--- test/SlackChannelsSpec.scala
+++ test/SlackChannelsSpec.scala
@@ -1,3 +1,4 @@
+import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
@@ -35,13 +36,14 @@ class SlackChannelsSpec extends PlaySpec with MockitoSugar {
     val slackConversation = SlackConversation.defaultFor(channelId, "test-channel")
 
     "return the ID if getInfo finds the conversation with or without the channel formatting junk" in new TestContext {
+      val formattedChannel = s"<#${slackConversation.id}|test-channel>"
       when(channels.client.conversationInfo(channelId)).thenReturn(Future.successful(Some(slackConversation)))
       await(channels.maybeIdFor(channelId)) mustBe Some(channelId)
-      await(channels.maybeIdFor(s"<#${slackConversation.id}|test-channel>")) mustBe Some(channelId)
+      await(channels.maybeIdFor(formattedChannel)) mustBe Some(channelId)
     }
 
     "map a name to an ID if there's a same-name item in getList, with or without the # prefix and formatting junk" in new TestContext {
-      when(channels.client.conversationInfo(channelId)).thenReturn(Future.successful(None))
+      when(channels.client.conversationInfo(any[String])).thenReturn(Future.successful(None))
       when(channels.client.listConversations()).thenReturn(Future.successful(Seq(slackConversation)))
       await(channels.maybeIdFor("test-channel")) mustBe Some(channelId)
       await(channels.maybeIdFor("#test-channel")) mustBe Some(channelId)
