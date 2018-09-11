d7042549144f472437c6254202d565fd132a3da2
Luke Andrews <luke@attaboy.ca>
2018-09-10 23:54:37 -0400

    When Slack conversations names became Option[String]s, maybeIdFor was never matching on names properly anymore

--- test/SlackChannelsSpec.scala
+++ test/SlackChannelsSpec.scala
@@ -1,8 +1,12 @@
+import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
+import play.api.test.Helpers._
 import services.slack.SlackApiClient
-import utils.SlackChannels
+import support.TestContext
+import utils.{SlackChannels, SlackConversation}
 
+import scala.concurrent.Future
 
 class SlackChannelsSpec extends PlaySpec with MockitoSugar {
 
@@ -24,6 +28,24 @@ class SlackChannelsSpec extends PlaySpec with MockitoSugar {
       channels.unformatChannelText(channelId) mustBe channelId
     }
 
+  }
+
+  "maybeIdFor" should {
+
+    val slackConversation = SlackConversation.defaultFor(channelId, "test-channel")
+
+    "return the ID if getInfo finds the conversation with or without the channel formatting junk" in new TestContext {
+      when(channels.client.conversationInfo(channelId)).thenReturn(Future.successful(Some(slackConversation)))
+      await(channels.maybeIdFor(channelId)) mustBe Some(channelId)
+      await(channels.maybeIdFor(s"<#${slackConversation.id}|test-channel>")) mustBe Some(channelId)
+    }
+
+    "map a name to an ID if there's a same-name item in getList, with or without the # prefix and formatting junk" in new TestContext {
+      when(channels.client.conversationInfo(channelId)).thenReturn(Future.successful(None))
+      when(channels.client.listConversations()).thenReturn(Future.successful(Seq(slackConversation)))
+      await(channels.maybeIdFor("test-channel")) mustBe Some(channelId)
+      await(channels.maybeIdFor("#test-channel")) mustBe Some(channelId)
+    }
   }
 
 }
