0c80829e3adbb7fc1a8016c7516216ed3ccb46af
Andrew Catton <andrew@catton.ca>
2018-09-21 10:50:46 -0400

    Make sure querying BehaviorGroupDeployments leaves out tombstoned BehaviorGroups

--- app/models/behaviors/behaviorgroupdeployment/BehaviorGroupDeploymentQueries.scala
+++ app/models/behaviors/behaviorgroupdeployment/BehaviorGroupDeploymentQueries.scala
@@ -6,7 +6,8 @@ import models.behaviors.behaviorgroup.BehaviorGroupQueries
 
 object BehaviorGroupDeploymentQueries {
 
-  val all = TableQuery[BehaviorGroupDeploymentsTable]
+  // Don't find deployments for tombstoned groups
+  val all = TableQuery[BehaviorGroupDeploymentsTable].join(BehaviorGroupQueries.all).on(_.groupId === _.id).map(_._1)
   val allWithUser = all.join(UserQueries.all).on(_.userId === _.id)
 
   private def uncompiledAllForBehaviorGroupQuery(groupId: Rep[String]) = {
