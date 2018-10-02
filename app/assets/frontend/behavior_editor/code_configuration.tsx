113cb40a674f78323fdd88a3db24c64fd5fe1343
Luke Andrews <luke@attaboy.ca>
2018-10-02 18:56:48 -0400

    Move editor's ellipsis object code definitions to their own utility module

--- app/assets/frontend/behavior_editor/code_configuration.tsx
+++ app/assets/frontend/behavior_editor/code_configuration.tsx
@@ -16,6 +16,7 @@ import OAuthApplicationUnusedNotificationData from "../models/notifications/oaut
 import AWSUnusedNotificationData from "../models/notifications/aws_unused_notification_data";
 import NotificationData from "../models/notifications/notification_data";
 import autobind from "../lib/autobind";
+import EllipsisObjectDefinitions from "../code_editor/definitions/ellipsis";
 
 interface Props {
   sectionNumber: string,
@@ -132,139 +133,17 @@ class CodeConfiguration extends React.Component<Props, State> {
     }
 
     getCodeDefinitions(): string {
+      const ellipsisObjectDefinitions = this.props.systemParams.includes("ellipsis") ? EllipsisObjectDefinitions.buildFor({
+        requiredAWSConfigs: this.props.requiredAWSConfigs,
+        oauthApiApplications: this.props.oauthApiApplications,
+        envVariableNames: this.props.envVariableNames
+      }) : "";
       return `
-declare function require(path: string): any;
-
-${this.props.systemParams.includes("ellipsis") ? `
-
-declare namespace ellipsis {
-
-  export interface ActionArg {
-    name: string
-    value: any
-  }
-
-  export interface NextAction {
-    actionName: string
-    args?: ActionArg[]
-  }
-
-  export interface ActionChoice {
-    label: string
-    actionName: string
-    args?: ActionArg[]
-    allowOthers?: boolean
-    allowMultipleSelections?: boolean
-    quiet?: boolean
-  }
-
-  export interface InlineFileAttachment {
-    content: string
-    filetype?: string
-  }
-
-  export interface NamedFileAttachment {
-    filename: string
-    filetype?: string
-  }
-
-  export type FileAttachment = InlineFileAttachment | NamedFileAttachment
-
-  export interface SuccessOptions {
-    next?: NextAction
-    choices?: ActionChoice[]
-    files?: FileAttachment[]
-  }
-
-  export interface AWSConfig {
-    accessKeyId: string
-    secretAccessKey: string
-    region: string
-  }
-
-  export interface OAuthLink {
-    externalSystem: string
-    token: string
-    oauthToken: string
-  }
-
-  export interface UserInfo {
-    links: OAuthLink[]
-    messageInfo: MessageInfo
-  }
-
-  export interface UserData {
-    context: string
-    userName: string
-    ellipsisUserId?: string
-    userIdForContext?: string
-    fullName?: string
-    email?: string
-    timeZone?: string
-  }
-
-  export interface MessageInfo {
-    text: string
-    medium: string
-    channel?: string
-    thread?: string
-    userId: string
-    usersMentioned: UserData[]
-    permalink?: string
-    details: ContextMessageDetails
-  }
-
-  export interface ContextMessageDetails {
-    channelMembers?: string[]
-    channelName?: string
-    name?: string
-    profile?: ContextUserProfile
-  }
-
-  export interface ContextUserProfile {
-    displayName?: string
-    firstName?: string
-    lastName?: string
-    realName?: string
-    email?: string
-    phone?: string
-  }
-  
-  function success(successResult: any, options?: SuccessOptions): void
-
-  function noResponse(): void
-
-  function error(error: Error | string, options?: {
-    userMessage?: string
-  })
-
-  const accessTokens: {
-    ${this.props.oauthApiApplications.map((ea) => `${ea.nameInCode}: string`).join(",\n")}
-  }
-
-  const env: {
-    ${this.props.envVariableNames.map((ea) => `${ea}: string`).join(",\n")}
-  }
-
-  const aws: {
-    ${this.props.requiredAWSConfigs.map(ea => `${ea.nameInCode}: AWSConfig`).join(",\n")}
-  }
-
-  const teamInfo: {
-    links: OAuthLink[]
-    botName: string
-    botUserIdForContext: string
-    timeZone: string
-  }
-
-  const userInfo: UserInfo & UserData
-}
-` : ""}
+${ellipsisObjectDefinitions}
 
 ${this.props.inputs.map(ea => {
-  return `declare var ${ea.name} = any`;
+  return `declare var ${ea.name}: any`;
 }).join("\n")}
-
 `
     }
 
