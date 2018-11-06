daf96fd5d1f36807463a18e0a7211c4d22193149
Luke Andrews <luke@attaboy.ca>
2018-11-06 15:04:33 -0500

    Use LfuCache for getting message permalinks from Slack API to avoid repetitive requests

--- app/models/behaviors/events/EventContext.scala
+++ app/models/behaviors/events/EventContext.scala
@@ -12,6 +12,7 @@ import models.team.Team
 import play.api.Logger
 import play.api.libs.json.{JsObject, JsValue, Json}
 import services.DefaultServices
+import services.caching.SlackMessagePermalinkCacheKey
 import services.slack.SlackApiError
 import slick.dbio.DBIO
 import utils.{SlackChannels, SlackMessageReactionHandler, SlackMessageSender, UploadFileSpec}
@@ -230,12 +231,19 @@ case class SlackEventContext(
     } yield maybeTs
   }
 
+  private def maybePermalinkFunctionFor(key: SlackMessagePermalinkCacheKey, services: DefaultServices): SlackMessagePermalinkCacheKey => Future[Option[String]] = {
+    (key) => {
+      val client = services.slackApiService.clientFor(profile)
+      client.permalinkFor(key.channel, key.messageTs)
+    }
+  }
+
   def maybePermalinkFor(
                           messageTs: String,
                           services: DefaultServices
                         )(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[Option[String]] = {
-    val client = services.slackApiService.clientFor(profile)
-    client.permalinkFor(channel, messageTs)
+    val key = SlackMessagePermalinkCacheKey(messageTs, channel, profile.slackTeamId)
+    services.cacheService.getSlackPermalinkForMessage(key, maybePermalinkFunctionFor(key, services))
   }
 
   def reactionHandler(eventualResults: Future[Seq[BotResult]], maybeMessageTs: Option[String], services: DefaultServices)
