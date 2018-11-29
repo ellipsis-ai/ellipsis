85b27e42a56f911cf43742af6b26a49362b7f448
Luke Andrews <luke@attaboy.ca>
2018-11-28 19:11:04 -0500

    Convert the Emoji picker with all the fixings to its own component so we can re-use it elsewhere

--- app/assets/frontend/behavior_editor/reaction_trigger_input.tsx
+++ app/assets/frontend/behavior_editor/reaction_trigger_input.tsx
@@ -1,25 +1,9 @@
 import * as React from 'react';
-import { Picker } from 'emoji-mart';
-import { Emoji } from 'emoji-mart';
 import DeleteButton from '../shared_ui/delete_button';
 import FormInput from '../form/input';
 import Trigger, {TriggerInterface} from '../models/trigger';
-import Button from "../form/button";
 import autobind from "../lib/autobind";
-import DropdownContainer from "../shared_ui/dropdown_container";
-
-interface EmojiInterface {
-  id: string
-  name: string
-  colons: string
-  text: string
-  emoticons: Array<string>
-  skin?: Option<number>
-  native?: Option<string>
-  custom?: Option<boolean>
-  imageUrl?: Option<string>
-  short_names?: Array<string>
-}
+import EmojiInput, {EmojiInterface} from "../form/emoji_input";
 
 interface Props {
   existingTriggerEmojiIds: Array<string>
@@ -30,10 +14,6 @@ interface Props {
   isShowingEmojiPicker: boolean
   onToggleEmojiPicker: () => void
 }
-
-const EMOJI_SIZE = 32;
-const EMOJI_SHEET_SIZE = 64;
-const EMOJI_SET = "twitter";
 
 class ReactionTriggerInput extends React.Component<Props> {
   input: Option<FormInput>;
@@ -76,36 +56,17 @@ class ReactionTriggerInput extends React.Component<Props> {
     }
   }
 
-  renderSelectedEmoji() {
-    const text = this.props.trigger.text;
-    if (text) {
-      return (
-        <Emoji
-          native={true}
-          emoji={{ id: this.props.trigger.text, skin: 3 }}
-          size={EMOJI_SIZE}
-          sheetSize={EMOJI_SHEET_SIZE}
-          set={EMOJI_SET}
-        />
-      );
-    } else {
-      return (
-        <span className="type-icon type-disabled">?</span>
-      );
-    }
-  }
-
   renderEmojiText(): React.ReactElement<HTMLSpanElement> {
     const text = this.props.trigger.text;
-    if (text && text.trim().length > 0) {
-      return (
-        <span className="type-monospace">:{text.trim()}:</span>
-      );
-    } else {
-      return (
-        <span className="type-disabled">(None)</span>
-      );
-    }
+    return (
+      <span className="display-inline-block align-m type-weak type-s">
+        {text && text.trim().length > 0 ? (
+          <span className="type-monospace">:{text.trim()}:</span>
+        ) : (
+          <span className="type-disabled">(None)</span>
+        )}
+      </span>
+    );
   }
 
   toggleReactionPicker(): void {
@@ -126,29 +87,14 @@ class ReactionTriggerInput extends React.Component<Props> {
       <div className="border border-light bg-white mrm mbm display-inline-block">
         <div className="columns columns-elastic">
           <div className="column column-expand pvs plm prn">
-            {this.props.isShowingEmojiPicker ? (
-              <DropdownContainer>
-                <div className="popup popup-shadow popup-demoted">
-                  <Picker
-                    native={true}
-                    set={EMOJI_SET}
-                    onClick={this.onClickEmoji}
-                    sheetSize={EMOJI_SHEET_SIZE}
-                    emojiSize={24}
-                    autoFocus={true}
-                    title={this.props.trigger.text || "Pick your emoji…"}
-                    emoji={this.props.trigger.text}
-                    showPreview={false}
-                    perLine={8}
-                    emojisToShowFilter={this.filterExistingEmoji}
-                  />
-                </div>
-              </DropdownContainer>
-            ) : null}
-            <Button className="button-dropdown-trigger button-dropdown-trigger-borderless pvn" onClick={this.toggleReactionPicker} stopPropagation={true}>
-              <span className="display-inline-block align-m mrm height-icon width-icon align-c">{this.renderSelectedEmoji()}</span>
-              <span className="display-inline-block align-m type-weak type-s">{this.renderEmojiText()}</span>
-            </Button>
+            <EmojiInput
+              onClickEmoji={this.onClickEmoji}
+              value={this.props.trigger.text}
+              filterEmoji={this.filterExistingEmoji}
+              emojiAsText={this.renderEmojiText()}
+              pickerVisible={this.props.isShowingEmojiPicker}
+              onTogglePicker={this.toggleReactionPicker}
+            />
           </div>
           <div className="column column-shrink align-m">
             <DeleteButton onClick={this.props.onDelete} />
