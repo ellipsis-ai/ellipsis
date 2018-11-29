51c2658a3d85118c7eb6bff262d57c94da9c6705
Andrew Catton <andrew@catton.ca>
2018-11-29 10:16:31 -0500

    Need an actionName for text inputs too

--- app/models/behaviors/events/ms_teams/MSTeamsMessageTextInput.scala
+++ app/models/behaviors/events/ms_teams/MSTeamsMessageTextInput.scala
@@ -1,10 +1,10 @@
 package models.behaviors.events.ms_teams
 
 import models.behaviors.events.MessageMenu
-import play.api.libs.json.JsNull
+import play.api.libs.json.Json
 import services.ms_teams.apiModels._
 
 case class MSTeamsMessageTextInput(name: String) extends MSTeamsMessageAction with MessageMenu {
   def bodyElements: Seq[CardElement] = Seq(InputText(name))
-  def actionElements: Seq[CardElement] = Seq(ActionSubmit("Send response", JsNull))
+  def actionElements: Seq[CardElement] = Seq(ActionSubmit("Send response", Json.obj("actionName" -> name)))
 }
