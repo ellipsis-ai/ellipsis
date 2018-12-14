9f3190cf160a82ac2a2a48c25e38d2266621d63a
Luke Andrews <luke@attaboy.ca>
2018-12-13 21:30:03 -0500

    Add mostly-empty type info for untyped modules we're using

---
+++ app/assets/frontend/types/modules.d.ts
@@ -0,0 +1,9 @@
+// TODO: Write types for external modules we're using
+declare module "emoji-mart";
+declare module "javascript-debounce";
+declare module "monaco-editor/esm/vs/language/typescript/lib/lib" {
+  export const lib_es5_dts: string;
+  export const lib_es2015_dts: string;
+}
+declare module "urijs";
+declare module "uuid";
