17e38328513754ff634ca957181cd57171d6397b
Andrew Catton <andrew@catton.ca>
2018-12-19 15:01:01 -0500

    Make slack-style mentions like <@username> work in MS Teams too

--- app/models/MSTeamsMessageFormatter.scala
+++ app/models/MSTeamsMessageFormatter.scala
@@ -1,7 +1,22 @@
 package models
 
+import java.util.regex.Matcher
+
+import json.UserData
 import models.behaviors.templates.{ChatPlatformRenderer, MSTeamsRenderer}
 
 object MSTeamsMessageFormatter extends ChatPlatformMessageFormatter {
   def newRendererFor(builder: StringBuilder): ChatPlatformRenderer = new MSTeamsRenderer(builder)
+
+  def convertUsernamesToLinks(formattedText: String, userList: Set[UserData]): String = {
+    userList.toSeq.foldLeft(formattedText) { (text, user) =>
+      user.userIdForContext.map { uid =>
+        val linkRegex = raw"""<@$uid>""".r
+        val replacement = user.formattedLink.getOrElse {
+          s"@${user.userNameOrDefault}"
+        }
+        linkRegex.replaceAllIn(text, replacement)
+      }.getOrElse(text)
+    }
+  }
 }
