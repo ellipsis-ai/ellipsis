ebd75a622c90067102aa426b9b7c1a5c826f1adb
Andrew Catton <andrew@catton.ca>
2019-02-27 16:12:35 -0500

    Add a TODO

--- app/services/ms_teams/apiModels/FirstMessageInfo.scala
+++ app/services/ms_teams/apiModels/FirstMessageInfo.scala
@@ -11,7 +11,7 @@ case class FirstMessageInfo(
 
   val maybeResponseUrl: Option[String] = None
 
-  val serviceUrl: String = "https://smba.trafficmanager.net/amer/"
+  val serviceUrl: String = "https://smba.trafficmanager.net/amer/" // TODO: theoretically this should be saved away from a recent event
   val getPrivateConversationIdUrl: String = s"$serviceUrl/v3/conversations"
 
   val maybeTenantId: Option[String] = channelData.tenant.map(_.id)
