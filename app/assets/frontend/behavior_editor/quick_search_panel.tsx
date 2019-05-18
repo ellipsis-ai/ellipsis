aba6afb5259969d10121b168c17486c6eaa53927
Luke Andrews <luke@attaboy.ca>
2019-05-17 20:41:16 -0400

    Rudimentary code search in the quick search panel

--- app/assets/frontend/behavior_editor/quick_search_panel.tsx
+++ app/assets/frontend/behavior_editor/quick_search_panel.tsx
@@ -6,6 +6,7 @@ import Editable from "../models/editable";
 import Button from "../form/button";
 import SearchWithResults, {SearchOption} from "../form/search_with_results";
 import Sort from "../lib/sort";
+import Textarea from "../form/textarea";
 
 interface Props {
   group: BehaviorGroup
@@ -19,6 +20,7 @@ interface State {
     value: string
   }>
   currentResultValue: string,
+  currentSearchValue: string,
   hasMatches: boolean
 }
 
@@ -35,6 +37,7 @@ class QuickSearchPanel extends React.Component<Props, State> {
     this.state = {
       results: [],
       currentResultValue: "",
+      currentSearchValue: "",
       hasMatches: false
     };
   }
@@ -45,6 +48,7 @@ class QuickSearchPanel extends React.Component<Props, State> {
     this.setState({
       results: newResults,
       currentResultValue: firstResult || "",
+      currentSearchValue: newValue,
       hasMatches: Boolean(firstResult)
     });
   }
@@ -73,21 +77,26 @@ class QuickSearchPanel extends React.Component<Props, State> {
     });
   }
 
-  tokenize(text: string): Array<string> {
-    return text.trim().toLowerCase().replace(/\s+/g, " ").split(" ");
+  tokenize(text: string, forCode: boolean): Array<string> {
+    const searchPattern = forCode ? /\W+/g : /\s+/g;
+    return text.trim().toLowerCase().replace(searchPattern, " ").split(" ");
   }
 
-  anyTokenMatches(text: string, searchText: string): boolean {
-    return this.tokenize(text).some((ea) => ea.startsWith(searchText));
+  anyTokenMatches(text: string, searchText: string, forCode: boolean): boolean {
+    return this.tokenize(text, forCode).some((ea) => ea.startsWith(searchText));
   }
 
   actionMatchesSearch(action: BehaviorVersion, searchText: string): boolean {
     return this.editableMatchesSearch(action, searchText) ||
-      action.triggers.some((trigger) => this.anyTokenMatches(trigger.getText(), searchText));
+      action.triggers.some((trigger) => this.anyTokenMatches(trigger.getText(), searchText, false)) ||
+      this.anyTokenMatches(action.functionBody, searchText, false) ||
+      this.anyTokenMatches(action.functionBody, searchText, true);
   }
 
   editableMatchesSearch(editable: Editable, searchText: string): boolean {
-    return this.anyTokenMatches(editable.getName(), searchText);
+    return this.anyTokenMatches(editable.getName(), searchText, false) ||
+      this.anyTokenMatches(editable.functionBody, searchText, false) ||
+      this.anyTokenMatches(editable.functionBody, searchText, true);
   }
 
   rankFor(text: string, searchText: string): number {
@@ -129,24 +138,69 @@ class QuickSearchPanel extends React.Component<Props, State> {
     return Sort.arrayAlphabeticalBy(actionOptions.concat(otherOptions), (ea) => `${ea.rank}${ea.name}`);
   };
 
+  getCurrentSelectionFunctionLines(): {
+    start: number,
+    lines: Array<string>
+  } {
+    const id = this.state.currentResultValue;
+    const editable = this.props.group.behaviorVersions.find((ea) => ea.getPersistentId() === id) ||
+      this.props.group.libraryVersions.find((ea) => ea.getPersistentId() === id);
+    if (editable) {
+      const functionLines = editable.functionBody.split("\n");
+      const firstRelevantLine = functionLines.findIndex((line) => line.toLowerCase().includes(this.state.currentSearchValue));
+      if (firstRelevantLine !== -1) {
+        const firstLine = Math.max(firstRelevantLine - 1, 0);
+        return {
+          start: firstLine,
+          lines: functionLines.slice(firstLine, firstLine + 10)
+        };
+      } else {
+        return {
+          start: 1,
+          lines: functionLines.slice(0, 10)
+        };
+      }
+    } else {
+      return {
+        start: 0,
+        lines: []
+      };
+    }
+  }
+
   render() {
+    const functionResult = this.getCurrentSelectionFunctionLines();
     return (
       <div className="box-action phn">
         <div className="container container-c">
-          <h4 className="mvn">Switch to any action, data type, library or test in this skill</h4>
+          <h4 className="mvn">Search any action, data type, library or test in this skill</h4>
 
-          <SearchWithResults
-            ref={(el) => this.searchInput = el}
-            placeholder={"Search by name"}
-            value={this.state.currentResultValue}
-            options={this.state.results}
-            isSearching={false}
-            noMatches={!this.state.hasMatches}
-            onChangeSearch={this.onChangeSearchText}
-            onSelect={this.onSelectResult}
-            onEnterKey={this.onSelect}
-            onEscKey={this.onEscKey}
-          />
+          <div className="columns">
+            <div className="column column-one-half">
+              <SearchWithResults
+                ref={(el) => this.searchInput = el}
+                placeholder={"Search by name, trigger or code"}
+                value={this.state.currentResultValue}
+                options={this.state.results}
+                isSearching={false}
+                noMatches={!this.state.hasMatches}
+                onChangeSearch={this.onChangeSearchText}
+                onSelect={this.onSelectResult}
+                onEnterKey={this.onSelect}
+                onEscKey={this.onEscKey}
+              />
+            </div>
+            <div className="column column-one-half">
+              <div className="display-nowrap display-overflow-hidden border pas type-monospace type-s">
+                {functionResult.lines.map((ea, index) => (
+                  <div key={`line${index}`}>
+                    <span className="bg-light type-disabled mrs">{functionResult.start + index + 1}</span>
+                    <pre className="display-inline">{ea}</pre>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
 
           <Button className="button-primary mrs" onClick={this.onSelect} disabled={!this.state.currentResultValue}>Switch to selection</Button>
           <Button onClick={this.onDone}>Cancel</Button>
