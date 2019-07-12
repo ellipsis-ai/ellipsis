bfd10ba2f427f36f079b87a2054db58dbaddbcaa
Luke Andrews <luke@attaboy.ca>
2019-07-12 13:53:28 -0400

    Add some controller actions for fetching a list of users and setting a managed skill contact

--- app/controllers/BehaviorGroupConfigController.scala
+++ app/controllers/BehaviorGroupConfigController.scala
@@ -5,12 +5,14 @@ import com.google.inject.Provider
 import com.mohiva.play.silhouette.api.Silhouette
 import javax.inject.Inject
 import json.Formatting._
-import json.ScheduledActionsConfig
+import json.{ScheduledActionsConfig, UserData}
 import models.ViewConfig
+import models.accounts.user.User
 import models.silhouette.EllipsisEnv
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
 import services.DefaultServices
+import utils.FutureSequencer
 import views.html.helper.CSRF
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -71,5 +73,53 @@ class BehaviorGroupConfigController @Inject()(
         }
       }
     }
+  }
+
+  private case class TeamUserData(teamId: String, users: Seq[UserData])
+
+  private implicit val teamUserDataFormat = Json.format[TeamUserData]
+
+  def teamUserData(teamId: String) = silhouette.SecuredAction.async { implicit request =>
+    val user = request.identity
+    for {
+      teamAccess <- dataService.users.teamAccessFor(user, Some(teamId))
+      result <- teamAccess.maybeTargetTeam.map { team =>
+        for {
+          users <- dataService.users.allFor(team)
+          usersData <- FutureSequencer.sequence(users, (ea: User) =>
+            dataService.users.userDataFor(ea, team)
+          )
+        } yield {
+          Ok(Json.toJson(TeamUserData(teamId, usersData)))
+        }
+      }.getOrElse {
+        Future.successful(NotFound("Team not found"))
+      }
+    } yield result
+  }
+
+  private case class SetContactInfo(contactId: Option[String])
+
+  private implicit val setContactInfoFormat = Json.format[SetContactInfo]
+
+  def setManagedContact(groupId: String) = silhouette.SecuredAction(parse.json).async { request =>
+    request.body.validate[SetContactInfo].fold(
+      errors => {
+        Future.successful(BadRequest(Json.toJson(errors.map(ea => (ea._1.toString(), ea._2.map(_.message))))))
+      },
+      contactInfo => {
+        for {
+          maybeGroup <- dataService.behaviorGroups.findWithoutAccessCheck(groupId)
+          maybeContact <- contactInfo.contactId.map { contactId =>
+            dataService.users.find(contactId)
+          }.getOrElse(Future.successful(None))
+          result <- maybeGroup.map { group =>
+            dataService.managedBehaviorGroups.ensureFor(group, maybeContact).map { _ =>
+              Ok(Json.toJson(contactInfo))
+            }
+          }.getOrElse(Future.successful(NotFound(s"Skill not found: ${groupId}")))
+        } yield result
+      }
+    )
   }
 }
