b55df8d2f8ea2d47f5e63be7c5cfcc0787284374
Luke Andrews <luke@attaboy.ca>
2019-07-12 10:10:09 -0400

    Andrew doesn't like deleting rows

--- conf/evolutions/default/154.sql
+++ conf/evolutions/default/154.sql
@@ -1,11 +1,6 @@
 # --- !Ups
 
-BEGIN;
-
-DELETE FROM invocation_log_entries WHERE runtime_in_milliseconds IS NULL;
 ALTER TABLE invocation_log_entries ALTER runtime_in_milliseconds SET NOT NULL;
-
-COMMIT;
 
 # --- !Downs
 
